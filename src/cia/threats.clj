abf64ffc0ce58a3c5958612722e8b91a2bcb76b7
Steve Sloan <stevsloa@cisco.com>
2016-01-28 08:58:33 -0800

    First pass at Indident schema

--- src/cia/threats.clj
+++ src/cia/threats.clj
@@ -1,6 +1,6 @@
 (ns cia.threats
   (:require [schema.core :as s]
-            [cia.models :refer [Observable ObservableType Time URI Confidence Reference ID]]
+            [cia.models :refer [Observable Time URI Confidence Reference ID Indicator]]
             [ring.swagger.schema :refer [coerce!]]))
 ;;mutable
 (s/defschema TTP
@@ -13,7 +13,7 @@
    :expires Time
 
    :description s/Str
-   :short_description s/Str 
+   :short_description s/Str
 
    :intended_effect s/Str ;; typed
 
@@ -33,7 +33,7 @@
    :expires Time
 
    :description s/Str
-   :short_description s/Str 
+   :short_description s/Str
    })
 
 ;;mutable
@@ -45,7 +45,7 @@
    :type  s/Str
    :timestamp Time
    :expires Time
-   
+
    :description s/Str
    :short_description s/Str
 
@@ -71,5 +71,57 @@
 
    :handling s/Str
    :related_COAs [Reference]
-   
+
+   })
+
+(def Scope
+  (s/either "inclusive" "exclusive"))
+
+(def SecurityCompromise
+  (s/enum "Yes" "No" "Suspected" "Unknown"))
+
+(s/defschema Incident
+  "See http://stixproject.github.io/data-model/1.2/incident/IncidentType/"
+  {:id ID
+   :timestamp Time
+
+   :description s/Str
+   :short_description s/Str
+   :status s/Str
+
+   (s/optional-key :categories) [s/Str]
+   (s/optional-key :reporter) s/Str
+   (s/optional-key :responder) s/Str
+   (s/optional-key :coordinator) s/Str
+   (s/optional-key :victim) s/Str
+
+   ;; affected assets ?
+   ;; impact assessment ?
+
+   ;; The seqs of elements below are squashed (they leave out
+   ;; structured data such as confidence and source for each element).
+   (s/optional-key :related_indicators) {:scope Scope
+                                         :indicators [Reference]}
+   (s/optional-key :related_observables) {:scope Scope
+                                          :observables [Reference]}
+   (s/optional-key :leveraged_ttps) {:scope Scope
+                                     :ttps [Reference]}
+   (s/optional-key :attributed_actors) {:scope Scope
+                                        :actors [Actor]}
+   (s/optional-key :related_incidents) {:scope Scope
+                                        :incidents [Reference]}
+   (s/optional-key :intended_effect) s/Str ;; typed?
+
+   :security_compromise SecurityCompromise
+   :discover_method s/Str ;; typed?
+
+   :coa_requested [Reference]
+   :coa_taken [Reference]
+
+   :confidence s/Str ;; typed?
+
+   ;; contact?
+   ;; history?
+   ;; information source?
+   ;; handling?
    })
