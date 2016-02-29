bd8cad5372b3ea364fcb4302d684ce38e12dd529
Steve Sloan <stevsloa@cisco.com>
2016-02-29 07:30:10 -0800

    Delete kruft

--- src/cia/stores/sql/store.clj
+++ src/cia/stores/sql/store.clj
@@ -14,16 +14,3 @@
     (judgement/select-judgements filter-map))
   (calculate-verdict [_ observable]
     (first (judgement/calculate-verdict observable))))
-
-;; (defrecord IndicatorStore [state]
-;;   IIndicatorStore
-;;   (create-indicator [_ new-indicator]
-;;     (handle-create-indicator state new-indicator))
-;;   (read-indicator [_ id]
-;;     (handle-read-indicator state id))g
-;;   (delete-indicator [_ id]
-;;     (handle-delete-indicator state id))
-;;   (list-indicators [_ filter-map]
-;;     (handle-list-indicators state filter-map))
-;;   (list-indicator-sightings [_ filter-map]
-;;     (handle-list-indicators-sightings state filter-map)))
