5f73c7a8fe8a2d554c6d804dea667c6e9faffaf3
Guillaume Buisson <gbuisson@cisco.com>
2016-03-02 19:50:31 +0100

    added es index test helpers

---
+++ test/cia/test_helpers/index.clj
@@ -0,0 +1,40 @@
+(ns cia.test-helpers.index
+  (:require [cia.store :as store]
+            [cia.stores.es.index :as es-index :refer [es-conn]]
+            [cia.stores.es.mapping :as mapping]
+            [cia.stores.es.store :as es-store]
+            [cia.test-helpers.core :as h]
+            [clojure.java.io :as io]))
+
+(defn fixture-init-index [test]
+  (try
+    (es-index/init!)
+    (es-index/delete! es-conn)
+    (es-index/create! es-conn)
+    (test)
+    (es-index/delete! es-conn)
+    (catch org.elasticsearch.client.transport.NoNodeAvailableException e
+      (do  (println "ES unavailable")
+           (test)))))
+
+(defn fixture-clean-index [f]
+  (es-index/flush! es-conn)
+  (f))
+
+(defn init-state [f]
+  (fn []
+    (f {:conn es-conn})))
+
+(def es-stores
+  {store/actor-store          (init-state es-store/->ActorStore)
+   store/judgement-store      (init-state es-store/->JudgementStore)
+   store/feedback-store       (init-state es-store/->FeedbackStore)
+   store/campaign-store       (init-state es-store/->CampaignStore)
+   store/coa-store            (init-state es-store/->COAStore)
+   store/exploit-target-store (init-state es-store/->ExploitTargetStore)
+   store/incident-store       (init-state es-store/->IncidentStore)
+   store/indicator-store      (init-state es-store/->IndicatorStore)
+   store/ttp-store            (init-state es-store/->TTPStore)})
+
+
+(def fixture-es-store (h/fixture-store es-stores))
