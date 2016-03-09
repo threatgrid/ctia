e1c310f9d402f4d7621397abf4f1b93cb3156996
Steve Sloan <stevsloa@cisco.com>
2016-03-09 09:19:22 -0800

    Reformatted Indicator for readability

--- src/cia/schemas/indicator.clj
+++ src/cia/schemas/indicator.clj
@@ -56,23 +56,72 @@
   (merge
    c/GenericStixIdentifiers
    {:valid_time c/ValidTime
-    (s/optional-key :alternate_ids) (describe [s/Str] "alternative identifier (or alias)")
-    (s/optional-key :version) (describe s/Num "schema version for this content")
-    (s/optional-key :negate) (describe s/Bool "specifies the absence of the pattern")
-    (s/optional-key :type) (describe [v/IndicatorType] "Specifies the type or types for this Indicator")
-    (s/optional-key :observable) (describe c/Observable "a relevant cyber observable for this Indicator")
-    (s/optional-key :judgements) (describe rel/RelatedJudgements "related Judgements for this Indicator")
-    (s/optional-key :composite_indicator_expression) CompositeIndicatorExpression
-    (s/optional-key :indicated_TTP) (describe rel/RelatedTTPs "the relevant TTP indicated by this Indicator")
-    (s/optional-key :likely_impact) (describe s/Str "likely potential impact within the relevant context if this Indicator were to occur")
-    (s/optional-key :suggested_COAs) (describe rel/RelatedCOAs "suggested Courses of Action")
-    (s/optional-key :confidence) (describe v/HighMedLow "level of confidence held in the accuracy of this Indicator")
-    (s/optional-key :sightings) (describe [Sighting] "a set of sighting reports")
-    (s/optional-key :related_indicators) (describe rel/RelatedIndicators "relationship between the enclosing indicator and a disparate indicator")
-    (s/optional-key :related_campaigns) (describe rel/RelatedCampaigns "references to related campaigns")
-    (s/optional-key :related_COAs) (describe rel/RelatedCOAs "related Courses of Actions for this cyber threat Indicator")
-    (s/optional-key :kill_chain_phases) (describe [s/Str] "relevant kill chain phases indicated by this Indicator") ;; simplified
-    (s/optional-key :test_mechanisms) (describe [s/Str] "Test Mechanisms effective at identifying the cyber Observables specified in this cyber threat Indicator") ;; simplified
+    (s/optional-key :alternate_ids)
+    (describe [s/Str]
+              "alternative identifier (or alias)")
+
+    (s/optional-key :version)
+    (describe s/Num
+              "schema version for this content")
+
+    (s/optional-key :negate)
+    (describe s/Bool
+              "specifies the absence of the pattern")
+
+    (s/optional-key :type)
+    (describe [v/IndicatorType]
+              "Specifies the type or types for this Indicator")
+
+    (s/optional-key :observable)
+    (describe c/Observable
+              "a relevant cyber observable for this Indicator")
+
+    (s/optional-key :judgements)
+    (describe rel/RelatedJudgements
+              "related Judgements for this Indicator")
+
+    (s/optional-key :composite_indicator_expression)
+    CompositeIndicatorExpression
+
+    (s/optional-key :indicated_TTP)
+    (describe rel/RelatedTTPs
+              "the relevant TTP indicated by this Indicator")
+
+    (s/optional-key :likely_impact)
+    (describe s/Str
+              "likely potential impact within the relevant context if this Indicator were to occur")
+
+    (s/optional-key :suggested_COAs)
+    (describe rel/RelatedCOAs
+              "suggested Courses of Action")
+
+    (s/optional-key :confidence)
+    (describe v/HighMedLow
+              "level of confidence held in the accuracy of this Indicator")
+
+    (s/optional-key :sightings)
+    (describe [Sighting]
+              "a set of sighting reports")
+
+    (s/optional-key :related_indicators)
+    (describe rel/RelatedIndicators
+              "relationship between the enclosing indicator and a disparate indicator")
+
+    (s/optional-key :related_campaigns)
+    (describe rel/RelatedCampaigns
+              "references to related campaigns")
+
+    (s/optional-key :related_COAs)
+    (describe rel/RelatedCOAs
+              "related Courses of Actions for this cyber threat Indicator")
+
+    (s/optional-key :kill_chain_phases) ;; simplified
+    (describe [s/Str]
+              "relevant kill chain phases indicated by this Indicator")
+
+    (s/optional-key :test_mechanisms) ;; simplified
+    (describe [s/Str]
+              "Test Mechanisms effective at identifying the cyber Observables specified in this cyber threat Indicator")
 
     ;; Extension fields:
     :producer s/Str
