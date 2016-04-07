d943a9d92721fbca1a645e549ce54c5071800a3e
Paula Gearon <pgearon@cisco.com>
2016-04-07 01:11:45 -0400

    Added a go loop to listen to events and log them

---
+++ src/ctia/logging.clj
@@ -0,0 +1,13 @@
+(ns ctia.logging
+  (:require [clojure.tools.logging :as log]
+            [clojure.core.async :as a :refer [go <!]]
+            [ctia.events :as e]
+            [schema.core :as s])
+  (:import [clojure.core.asyc.impl.protocols Channel]))
+
+(s/defn log-channel :- Channel
+  "Logging an event channel indefinitely. Returns the channel for a go loop that never ends."
+  [{m :mult :as ev} :- e/EventChannel]
+  (go (loop [ev (<! m)]
+        (log/info ev)
+        (recur (<! m)))))
