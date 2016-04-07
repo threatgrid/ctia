963a81a76c00fdee12d1ade4003d4aab2963a9cb
Paula Gearon <pgearon@cisco.com>
2016-04-07 12:15:53 -0400

    Corrected the tapping of the multichan

--- src/ctia/logging.clj
+++ src/ctia/logging.clj
@@ -1,6 +1,6 @@
 (ns ctia.logging
   (:require [clojure.tools.logging :as log]
-            [clojure.core.async :as a :refer [go <!]]
+            [clojure.core.async :as a :refer [go <! chan]]
             [ctia.events :as e]
             [schema.core :as s])
   (:import [clojure.core.asyc.impl.protocols Channel]))
@@ -8,6 +8,8 @@
 (s/defn log-channel :- Channel
   "Logging an event channel indefinitely. Returns the channel for a go loop that never ends."
   [{m :mult :as ev} :- e/EventChannel]
-  (go (loop [ev (<! m)]
-        (log/info "event: " ev)
-        (recur (<! m)))))
+  (let [events (chan)]
+    (tap m events)
+    (go (loop [ev (<! events)]
+          (log/info "event: " ev)
+          (recur (<! events))))))
