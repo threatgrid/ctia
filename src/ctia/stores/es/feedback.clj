d1465a1c4b6709e73f157d333877d27f22a0c4a4
Guillaume Buisson <guillaume.buisson@gmail.com>
2016-04-11 15:32:31 +0200

    Feedback ES coercion

--- src/ctia/stores/es/feedback.clj
+++ src/ctia/stores/es/feedback.clj
@@ -2,20 +2,28 @@
   (:import java.util.UUID)
   (:require
    [schema.core :as s]
+   [schema.coerce :as c]
+   [ring.swagger.coerce :as sc]
    [ctia.schemas.feedback :refer [Feedback
-                                 NewFeedback
-                                 realize-feedback]]
+                                  NewFeedback
+                                  StoredFeedback
+                                  realize-feedback]]
    [ctia.stores.es.document :refer [create-doc
-                                   delete-doc
-                                   search-docs]]))
+                                    delete-doc
+                                    search-docs]]))
 
 (def ^{:private true} mapping "feedback")
 
+(def coerce-stored-feedback
+  (c/coercer! (s/maybe StoredFeedback)
+              sc/json-schema-coercion-matcher))
+
 (defn handle-create-feedback [state realized-new-feedback _ _]
-  (create-doc (:conn state)
-              (:index state)
-              mapping
-              realized-new-feedback))
+  (-> (create-doc (:conn state)
+                  (:index state)
+                  mapping
+                  realized-new-feedback)
+      coerce-stored-feedback))
 
 (defn handle-delete-feedback [state id]
   (delete-doc (:conn state)
@@ -24,7 +32,8 @@
               id))
 
 (defn handle-list-feedback [state filter-map]
-  (search-docs (:conn state)
-               (:index state)
-               mapping
-               filter-map))
+  (->> (search-docs (:conn state)
+                    (:index state)
+                    mapping
+                    filter-map)
+       (map coerce-stored-feedback)))
