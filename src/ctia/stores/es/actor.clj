0ffa0f37addd8e49b8105ab39975d1d6e0abe376
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2016-04-11 17:28:18 +0200

    Centralize ES crud functions. Prelude to refactor Store Protocols

--- src/ctia/stores/es/actor.clj
+++ src/ctia/stores/es/actor.clj
@@ -1,56 +1,10 @@
 (ns ctia.stores.es.actor
   (:import java.util.UUID)
-  (:require
-   [schema.core :as s]
-   [schema.coerce :as c]
-   [ring.swagger.coerce :as sc]
-   [ctia.schemas.actor :refer [Actor
-                               NewActor
-                               StoredActor
-                               realize-actor]]
-   [ctia.stores.es.document :refer [create-doc
-                                    update-doc
-                                    get-doc
-                                    delete-doc
-                                    search-docs]]))
+  (:require [ctia.stores.es.crud :as crud]
+            [ctia.schemas.actor :refer [StoredActor]]))
 
-(def ^{:private true} mapping "actor")
-
-(def coerce-stored-actor
-  (c/coercer! (s/maybe StoredActor)
-              sc/json-schema-coercion-matcher))
-
-(defn handle-create-actor [state _ realized-actor]
-  (-> (create-doc (:conn state)
-                  (:index state)
-                  mapping
-                  realized-actor)
-      coerce-stored-actor))
-
-(defn handle-update-actor [state id login realized-actor]
-  (-> (update-doc (:conn state)
-                  (:index state)
-                  mapping
-                  id
-                  realized-actor)
-      coerce-stored-actor))
-
-(defn handle-read-actor [state id]
-  (-> (get-doc (:conn state)
-               (:index state)
-               mapping
-               id)
-      coerce-stored-actor))
-
-(defn handle-delete-actor [state id]
-  (delete-doc (:conn state)
-              (:index state)
-              mapping
-              id))
-
-(defn handle-list-actors [state judgement-store filter-map]
-  (->> (search-docs (:conn state)
-                    (:index state)
-                    mapping
-                    filter-map)
-       (map coerce-stored-actor)))
+(def handle-create-actor (crud/handle-create :actor StoredActor))
+(def handle-read-actor (crud/handle-read :actor StoredActor))
+(def handle-update-actor (crud/handle-update :actor StoredActor))
+(def handle-delete-actor (crud/handle-delete :actor StoredActor))
+(def handle-list-actors (crud/handle-find :actor StoredActor))
---
+++ src/ctia/stores/es/crud.clj
@@ -0,0 +1,81 @@
+(ns ctia.stores.es.crud
+  (:require [ctia.lib.es.index :refer [ESConnState]]
+            [schema.core :as s]
+            [schema.coerce :as c]
+            [ring.swagger.coerce :as sc]
+            [ctia.stores.es.document :refer [create-doc
+                                             update-doc
+                                             get-doc
+                                             delete-doc
+                                             search-docs]]))
+
+(defn- coerce-to-fn
+  [Model]
+  (c/coercer! Model sc/json-schema-coercion-matcher))
+
+(defn handle-create
+  "Generate an ES create handler using some mapping and schema"
+  [mapping Model]
+  (let [coerce! (coerce-to-fn (s/maybe Model))]
+    (s/fn :- (s/maybe Model)
+      [state :- ESConnState
+       _ :- s/Any ;; Depecrated; to refactor
+       realized :- Model]
+      (-> (create-doc (:conn state)
+                      (:index state)
+                      (name mapping)
+                      realized)
+          coerce!))))
+
+(defn handle-update
+  "Generate an ES update handler using some mapping and schema"
+  [mapping Model]
+  (let [coerce! (coerce-to-fn (s/maybe Model))]
+    (s/fn :- (s/maybe Model)
+      [state :- ESConnState
+       id :- s/Str
+       _ :- s/Any ;; Depecrated; to refactor
+       realized :- Model]
+      (-> (update-doc (:conn state)
+                      (:index state)
+                      (name mapping)
+                      id
+                      realized)
+          coerce!))))
+
+(defn handle-read
+  "Generate an ES read handler using some mapping and schema"
+  [mapping Model]
+  (let [coerce! (coerce-to-fn (s/maybe Model))]
+    (s/fn :- (s/maybe Model)
+      [state :- ESConnState
+       id :- s/Str]
+      (-> (get-doc (:conn state)
+                   (:index state)
+                   (name mapping)
+                   id)
+          coerce!))))
+
+(defn handle-delete
+  "Generate an ES delete handler using some mapping and schema"
+  [mapping Model]
+  (s/fn :- s/Bool
+    [state :- ESConnState
+     id :- s/Str]
+    (-> (delete-doc (:conn state)
+                    (:index state)
+                    (name mapping)
+                    id))))
+
+(defn handle-find
+  "Generate an ES find/list handler using some mapping and schema"
+  [mapping Model]
+  (let [coerce! (coerce-to-fn [Model])]
+    (s/fn :- [Model]
+      [state :- ESConnState
+       filter-map :- {s/Any s/Any}]
+      (-> (search-docs (:conn state)
+                       (:index state)
+                       (name mapping)
+                       filter-map)
+          coerce!))))
