18faa0c69f246f9bfcab265444f8427cb778ca31
Guillaume Buisson <guillaume.buisson@gmail.com>
2016-04-18 17:23:12 +0200

    fixed typo

--- test/ctia/http/routes/actor_test.clj
+++ test/ctia/http/routes/actor_test.clj
@@ -1,11 +1,11 @@
 (ns ctia.http.routes.actor-test
+  (:refer-clojure :exclude [get])
   (:require
    [clojure.test :refer [deftest is testing use-fixtures join-fixtures]]
    [ctia.test-helpers.core :refer [delete get post put] :as helpers]
    [ctia.test-helpers.fake-whoami-service :as whoami-helpers]
    [ctia.test-helpers.store :refer [deftest-for-each-store]]
    [ctia.test-helpers.auth :refer [all-capabilities]]))
-
 (use-fixtures :once (join-fixtures [helpers/fixture-schema-validation
                                     helpers/fixture-properties:clean
                                     whoami-helpers/fixture-server]))
