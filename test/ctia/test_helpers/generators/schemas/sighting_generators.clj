ea42576c99d29cdcdb67cd85eedbd4b42c88112a
Stephen Sloan <steve@stevesloan.com>
2016-05-16 11:21:23 -0700

    Refactored obj_to_event_test and fixed stored entity generators

--- test/ctia/test_helpers/generators/schemas/sighting_generators.clj
+++ test/ctia/test_helpers/generators/schemas/sighting_generators.clj
@@ -3,7 +3,7 @@
             [ctia.lib.time :as time]
             [ctia.schemas
              [common :as schemas-common]
-             [sighting :refer [NewSighting Sighting]]]
+             [sighting :refer [NewSighting StoredSighting]]]
             [ctia.test-helpers.generators.common
              :refer [complete leaf-generators maybe]
              :as common]
@@ -13,7 +13,7 @@
   (gen/fmap
    (fn [id]
      (complete
-      Sighting
+      StoredSighting
       {:id id}))
    (gen-id/gen-short-id-of-type :sighting)))
 
