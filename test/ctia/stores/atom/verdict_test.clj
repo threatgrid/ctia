0ed01aa6bbbd361b294abdccf74c5cfa674a4481
Paula Gearon <pgearon@cisco.com>
2016-05-20 17:49:59 -0400

    Added read test

--- test/ctia/stores/atom/verdict_test.clj
+++ test/ctia/stores/atom/verdict_test.clj
@@ -5,17 +5,39 @@
             [schema.test :as st]
             [schema-generators.generators :as g]
             [ctia.test-helpers.core :as test-helpers]
-            [ctia.schemas.verdict :refer [Verdict StoredVerdict realize-verdict]]))
+            [ctia.schemas.verdict :refer [Verdict StoredVerdict realize-verdict]])
+  (:import [java.util Date]))
 
 (use-fixtures :once st/validate-schemas)
 (use-fixtures :each (join-fixtures [test-helpers/fixture-properties:clean
                                     test-helpers/fixture-properties:atom-store
                                     test-helpers/fixture-ctia-fast]))
 
+(defn- test-equiv
+  [a b]
+  (let [a' (dissoc a :created :id)
+        b' (dissoc b :created :id :owner)]
+    (is (= a' b'))))
+
+(def one-second 1000)
+
+(defn time-since
+  [^Date d]
+  (- (.getTime (Date.)) (.getTime d)))
+
 (deftest store-test
   (let [verdicts (g/sample 5 StoredVerdict)]
     (doseq [v verdicts]
-      (println "Realizing " v)
       (let [verdict (select-keys v [:type :disposition :judgement_id :disposition_name])
             realized-verdict (realize-verdict verdict (:owner v))]
         (store/create-verdict @verdict-store realized-verdict)))))
+
+(deftest read-test
+  (let [verdicts (g/sample 5 StoredVerdict)]
+    (doseq [v verdicts]
+      (let [verdict (select-keys v [:type :disposition :judgement_id :disposition_name])
+            realized-verdict (realize-verdict verdict (:owner v))
+            {id :id} (store/create-verdict @verdict-store realized-verdict)
+            {created :created :as read-verdict} (store/read-verdict @verdict-store id)]
+        (test-equiv verdict read-verdict)
+        (is (> one-second (time-since created)))))))
