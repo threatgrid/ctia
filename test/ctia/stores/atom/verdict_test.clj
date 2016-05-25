d5991a5fafb71ed7f3110c60de8a4d1913d1f6aa
Paula Gearon <pgearon@cisco.com>
2016-05-24 21:46:42 -0400

    added tests to check for generated verdicts from judgements. Doesn't work yet, so using printlns to follow

--- test/ctia/stores/atom/verdict_test.clj
+++ test/ctia/stores/atom/verdict_test.clj
@@ -1,15 +1,21 @@
 (ns ctia.stores.atom.verdict-test
   (:require [ctia.stores.atom.store :as as]
-            [ctia.store :as store :refer [verdict-store]]
+            [ctia.store :as store :refer [verdict-store judgement-store]]
             [clojure.test :refer :all]
             [schema.test :as st]
             [schema-generators.generators :as g]
-            [ctia.test-helpers.core :as test-helpers]
-            [ctia.schemas.verdict :refer [Verdict StoredVerdict realize-verdict]])
+            [ctia.test-helpers.core :as test-helpers :refer [post]]
+            [ctia.test-helpers.fake-whoami-service :as whoami-helpers]
+            [ctia.test-helpers.auth :refer [all-capabilities]]
+            [ctia.schemas.verdict :refer [Verdict StoredVerdict realize-verdict]]
+            [ctia.schemas.judgement :refer [realize-judgement]])
   (:import [java.util Date]))
 
-(use-fixtures :once st/validate-schemas)
-(use-fixtures :each (join-fixtures [test-helpers/fixture-properties:clean
+(use-fixtures :once (join-fixtures [st/validate-schemas
+                                    test-helpers/fixture-properties:clean
+                                    whoami-helpers/fixture-server]))
+
+(use-fixtures :each (join-fixtures [whoami-helpers/fixture-reset-state
                                     test-helpers/fixture-properties:atom-store
                                     test-helpers/fixture-ctia-fast]))
 
@@ -41,3 +47,25 @@
             {created :created :as read-verdict} (store/read-verdict @verdict-store id)]
         (test-equiv verdict read-verdict)
         (is (> one-second (time-since created)))))))
+
+
+
+(deftest calculate-test
+  (test-helpers/set-capabilities! "foouser" "user" all-capabilities)
+  (whoami-helpers/set-whoami-response "45c1f5e3f05d0" "foouser" "user")
+  (let [observable {:value "http://observation.org/"
+                    :type "url"}
+        judgement {:observable observable
+                   :disposition 3
+                   :source "test"
+                   :priority 50
+                   :confidence "Low"
+                   :severity 100
+                   :valid_time {:start_time "1971-01-01T00:00:00.000-00:00"}
+                   :tlp "yellow"}
+        _ (println "Creating judgement")
+        response (post "ctia/judgement"
+                       :body judgement
+                       :headers {"api_key" "45c1f5e3f05d0"})]
+    (println "Response: " response)
+    (println "VERDICTS: " @verdict-store)))
