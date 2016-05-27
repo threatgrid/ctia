a036bc53a8522061ce78253620e618ef3021d31b
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2016-05-27 17:17:01 +0200

    Bulk create and read using the same Bulk data structure

--- src/ctia/http/handler.clj
+++ src/ctia/http/handler.clj
@@ -3,6 +3,7 @@
             [ctia.http.middleware.auth :as auth]
             [ctia.http.routes
              [actor :refer [actor-routes]]
+             [bulk :refer [bulk-routes]]
              [campaign :refer [campaign-routes]]
              [coa :refer [coa-routes]]
              [documentation :refer [documentation-routes]]
@@ -87,6 +88,7 @@
   (context "/ctia" []
     version-routes
     actor-routes
+    bulk-routes
     campaign-routes
     exploit-target-routes
     coa-routes
