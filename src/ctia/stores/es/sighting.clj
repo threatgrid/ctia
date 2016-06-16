a376c67886d1a1050abdf1b59dd3a68d532eae46
Guillaume Buisson <guillaume.buisson@gmail.com>
2016-06-16 16:25:43 +0200

    style fixes from feedback

--- src/ctia/stores/es/sighting.clj
+++ src/ctia/stores/es/sighting.clj
@@ -8,14 +8,25 @@
             [schema.core :as s]))
 
 (s/defschema ESStoredSighting
-  (st/merge StoredSighting
-            {:observables_hash [s/Str]}))
+  (st/assoc StoredSighting :observables_hash [s/Str]))
 
+(def ESStoredSightingList (list-response-schema ESStoredSighting))
+(def StoredSightingList (list-response-schema StoredSighting))
 (def es-coerce! (crud/coerce-to-fn [(s/maybe ESStoredSighting)]))
+(def create-fn (crud/handle-create :sighting ESStoredSighting))
+(def read-fn (crud/handle-read :sighting ESStoredSighting))
+(def update-fn (crud/handle-update :sighting ESStoredSighting))
+(def list-fn (crud/handle-find :sighting ESStoredSighting))
 
 (s/defn observable->observable-hash :- s/Str
-  [{:keys [type value] :as obsrvable :- Observable}]
+  "transform an observable to a hash"
+  [{:keys [type value] :as o :- Observable}]
   (str type ":" value))
+
+(s/defn obs->hashes :- [s/Str]
+  "transform a list of observables into hashes"
+  [observables :- [Observable]]
+  (map #(observable->observable-hash %) observables))
 
 (s/defn stored-sighting->es-stored-sighting :- (s/maybe ESStoredSighting)
   "adds an observables hash to a sighting"
@@ -31,44 +42,37 @@
 
 (s/defn handle-create-sighting :- StoredSighting
   [state realized]
-  (let [create-fn (crud/handle-create :sighting ESStoredSighting)
-        transformed (stored-sighting->es-stored-sighting realized)]
-    (-> (create-fn state transformed)
-        es-stored-sighting->stored-sighting)))
+  (->> (stored-sighting->es-stored-sighting realized)
+       (create-fn state)
+       es-stored-sighting->stored-sighting))
 
 (s/defn handle-read-sighting :- (s/maybe StoredSighting)
   [state id]
-  (let [read-fn (crud/handle-read :sighting ESStoredSighting)]
-    (-> (read-fn state id)
-        es-stored-sighting->stored-sighting)))
+  (es-stored-sighting->stored-sighting
+   (read-fn state id)))
 
 (s/defn handle-update-sighting :- StoredSighting
   [state id realized]
-  (let [update-fn (crud/handle-update :sighting ESStoredSighting)
-        transformed (stored-sighting->es-stored-sighting realized)]
-    (-> (update-fn state id transformed)
-        es-stored-sighting->stored-sighting)))
+  (->> (stored-sighting->es-stored-sighting realized)
+       (update-fn state id)
+       es-stored-sighting->stored-sighting))
 
 (def handle-delete-sighting (crud/handle-delete :sighting StoredSighting))
 
-(s/defn es-paginated-list->paginated-list :- (list-response-schema StoredSighting)
-  [paginated-list :- (list-response-schema ESStoredSighting)]
+(s/defn es-paginated-list->paginated-list :- StoredSightingList
+  [paginated-list :- ESStoredSightingList]
   (update-in paginated-list
              [:data]
              #(map es-stored-sighting->stored-sighting (es-coerce! %))))
 
-(defn handle-list-sightings
+(s/defn handle-list-sightings :- StoredSightingList
   [state filter-map params]
+  (es-paginated-list->paginated-list
+   (list-fn state filter-map params)))
 
-  (let [list-fn (crud/handle-find :sighting ESStoredSighting)
-        res (list-fn state filter-map params)]
-
-    (es-paginated-list->paginated-list res)))
-
-(def ^{:private true} mapping "sighting")
-
-(defn handle-list-sightings-by-observables
-  [state observables params]
-
-  (let [hashes (map #(observable->observable-hash %) observables)]
-    (handle-list-sightings state {:observables_hash hashes} params)))
+(s/defn handle-list-sightings-by-observables :- StoredSightingList
+  [state observables :- [Observable] params]
+  (handle-list-sightings state
+                         {:observables_hash
+                          (obs->hashes observables)}
+                         params))
