7ac5ae90253f4c6f607cfb08d8185ec5445220ff
Guillaume Buisson <guillaume.buisson@gmail.com>
2016-10-13 14:31:13 +0200

    fixed swagger-ui model signatures

--- src/ctia/schemas/core.clj
+++ src/ctia/schemas/core.clj
@@ -17,91 +17,91 @@
              [verdict :as vs]
              [vocabularies :as vocs]]
             [flanders.schema :as fs]
-            [schema.core :as s]))
+            [schema.core :refer [defschema Str Bool]]))
 
 ;; actor
-(def NewActor (fs/->schema-tree as/NewActor))
-(def StoredActor (fs/->schema-tree as/StoredActor))
+(defschema NewActor (fs/->schema-tree as/NewActor))
+(defschema StoredActor (fs/->schema-tree as/StoredActor))
 
 ;; campaign
-(def NewCampaign (fs/->schema-tree cs/NewCampaign))
-(def StoredCampaign (fs/->schema-tree cs/StoredCampaign))
+(defschema NewCampaign (fs/->schema-tree cs/NewCampaign))
+(defschema StoredCampaign (fs/->schema-tree cs/StoredCampaign))
 
 ;; coa
-(def NewCOA (fs/->schema-tree coas/NewCOA))
-(def StoredCOA (fs/->schema-tree coas/StoredCOA))
+(defschema NewCOA (fs/->schema-tree coas/NewCOA))
+(defschema StoredCOA (fs/->schema-tree coas/StoredCOA))
 
 ;; data-table
-(def NewDataTable (-> ds/NewDataTable
-                      fs/replace-either-with-any
-                      fs/->schema-tree))
+(defschema NewDataTable (-> ds/NewDataTable
+                            fs/replace-either-with-any
+                            fs/->schema-tree))
 
-(def StoredDataTable (-> ds/StoredDataTable
-                         fs/replace-either-with-any
-                         fs/->schema-tree))
+(defschema StoredDataTable (-> ds/StoredDataTable
+                               fs/replace-either-with-any
+                               fs/->schema-tree))
 
 ;; exploit-target
-(def NewExploitTarget (fs/->schema-tree es/NewExploitTarget))
-(def StoredExploitTarget (fs/->schema-tree es/StoredExploitTarget))
+(defschema NewExploitTarget (fs/->schema-tree es/NewExploitTarget))
+(defschema StoredExploitTarget (fs/->schema-tree es/StoredExploitTarget))
 
 ;; sighting
-(def NewSighting (fs/->schema-tree ss/NewSighting))
-(def StoredSighting (fs/->schema-tree ss/StoredSighting))
+(defschema NewSighting (fs/->schema-tree ss/NewSighting))
+(defschema StoredSighting (fs/->schema-tree ss/StoredSighting))
 
 ;; judgement
-(def NewJudgement (fs/->schema-tree js/NewJudgement))
-(def StoredJudgement (fs/->schema-tree js/StoredJudgement))
+(defschema NewJudgement (fs/->schema-tree js/NewJudgement))
+(defschema StoredJudgement (fs/->schema-tree js/StoredJudgement))
 
 ;; verdict
-(def Verdict (fs/->schema-tree vs/Verdict))
-(def StoredVerdict (fs/->schema-tree vs/StoredVerdict))
+(defschema Verdict (fs/->schema-tree vs/Verdict))
+(defschema StoredVerdict (fs/->schema-tree vs/StoredVerdict))
 
 ;; feedback
-(def NewFeedback (fs/->schema-tree feedbacks/NewFeedback))
-(def StoredFeedback (fs/->schema-tree feedbacks/StoredFeedback))
+(defschema NewFeedback (fs/->schema-tree feedbacks/NewFeedback))
+(defschema StoredFeedback (fs/->schema-tree feedbacks/StoredFeedback))
 
 ;; incident
-(def NewIncident (fs/->schema-tree is/NewIncident))
-(def StoredIncident (fs/->schema-tree is/StoredIncident))
+(defschema NewIncident (fs/->schema-tree is/NewIncident))
+(defschema StoredIncident (fs/->schema-tree is/StoredIncident))
 
 ;; indicator
-(def NewIndicator (-> ins/NewIndicator
-                      fs/replace-either-with-any
-                      fs/->schema-tree))
+(defschema NewIndicator (-> ins/NewIndicator
+                            fs/replace-either-with-any
+                            fs/->schema-tree))
 
-(def StoredIndicator (-> ins/StoredIndicator
+(defschema StoredIndicator (-> ins/StoredIndicator
+                               fs/replace-either-with-any
+                               fs/->schema-tree))
+
+;; ttp
+(defschema NewTTP (fs/->schema-tree ttps/NewTTP))
+(defschema StoredTTP (fs/->schema-tree ttps/StoredTTP))
+
+;; bundle
+(defschema NewBundle (-> bs/NewBundle
                          fs/replace-either-with-any
                          fs/->schema-tree))
 
-;; ttp
-(def NewTTP (fs/->schema-tree ttps/NewTTP))
-(def StoredTTP (fs/->schema-tree ttps/StoredTTP))
-
-;; bundle
-(def NewBundle (-> bs/NewBundle
-                   fs/replace-either-with-any
-                   fs/->schema-tree))
-
-(def StoredBundle (-> bs/StoredBundle
-                      fs/replace-either-with-any
-                      fs/->schema-tree))
+(defschema StoredBundle (-> bs/StoredBundle
+                            fs/replace-either-with-any
+                            fs/->schema-tree))
 
 ;; relationships
-(def RelatedIndicator (fs/->schema-tree rels/RelatedIndicator))
+(defschema RelatedIndicator (fs/->schema-tree rels/RelatedIndicator))
 
 ;; common
-(def Observable (fs/->schema-tree cos/Observable))
-(def Reference (fs/->schema-tree cos/Reference))
-(def ID (fs/->schema-tree cos/ID))
+(defschema Observable (fs/->schema-tree cos/Observable))
+(defschema Reference (fs/->schema-tree cos/Reference))
+(defschema ID (fs/->schema-tree cos/ID))
 
-(s/defschema VersionInfo
+(defschema VersionInfo
   "Version information for a specific instance of CTIA"
-  {:base s/Str
-   :version s/Str
-   :build s/Str
-   :beta s/Bool
-   :supported_features [s/Str]})
+  {:base Str
+   :version Str
+   :build Str
+   :beta Bool
+   :supported_features [Str]})
 
 ;; vocabularies
-(def ObservableTypeIdentifier
+(defschema ObservableTypeIdentifier
   (fs/->schema-tree vocs/ObservableTypeIdentifier))
