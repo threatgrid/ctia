18b7fff38c323716bc0d30cf3a4f7ab1950cb8b0
Stephen Sloan <steve@stevesloan.com>
2017-02-24 12:04:23 -0800

    With static auth enabled, allow reads for clients that don't have the secret

--- src/ctia/auth/static.clj
+++ src/ctia/auth/static.clj
@@ -1,31 +1,54 @@
 (ns ctia.auth.static
   (:require [clj-momo.lib.set :refer [as-set]]
-            [clojure.set :as set]
+            [clojure
+             [set :as set]
+             [string :as str]]
             [ctia
              [auth :refer [IIdentity IAuth] :as auth]
              [properties :as p]]))
 
-(def ^:private my-capabilities
+(def ^:private write-capabilities
   (set/difference auth/all-capabilities
                   #{:specify-id}))
 
-(defrecord Identity [name]
+(def ^:private read-only-capabilities
+  (set/difference (->> auth/all-capabilities
+                       (remove (fn [cap]
+                                 (some #(str/starts-with? (name cap) %)
+                                       ["create" "delete"])))
+                       set)
+                  #{:developer
+                    :specify-id}))
+
+(defrecord WriteIdentity [name]
   IIdentity
   (authenticated? [_]
     true)
   (login [_]
     name)
   (allowed-capabilities [_]
-    my-capabilities)
+    write-capabilities)
   (capable? [this required-capabilities]
     (set/subset? (as-set required-capabilities)
-                 my-capabilities)))
+                 write-capabilities)))
+
+(defrecord ReadOnlyIdentity []
+  IIdentity
+  (authenticated? [_]
+    true)
+  (login [_]
+    auth/not-logged-in-owner)
+  (allowed-capabilities [_]
+    read-only-capabilities)
+  (capable? [this required-capabilities]
+    (set/subset? (as-set required-capabilities)
+                 read-only-capabilities)))
 
 (defrecord AuthService [auth-config]
   IAuth
   (identity-for-token [_ token]
     (if (= token (get-in auth-config [:static :secret]))
-      (->Identity (get-in auth-config [:static :name]))
-      auth/denied-identity-singleton))
+      (->WriteIdentity (get-in auth-config [:static :name]))
+      (->ReadOnlyIdentity)))
   (require-login? [_]
     true))
