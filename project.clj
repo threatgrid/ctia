d1262a291297c46850f14a866fc5dece368da3ac
Stephen Sloan <steve@stevesloan.com>
2017-05-03 07:58:52 -0700

    Using CTIM version 0.4.9c

--- project.clj
+++ project.clj
@@ -42,7 +42,7 @@
                  ;; Schemas
                  [prismatic/schema ~schema-version]
                  [metosin/schema-tools ~schema-tools-version]
-                 [threatgrid/ctim "0.4.9b"]
+                 [threatgrid/ctim "0.4.9c"]
 
                  ;; Web server
                  ;; ring-swagger 0.22.10 provided by compojure-api
