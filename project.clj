c982a8d57be32237b90fc54f06792bedde86c741
Guillaume Buisson <guillaume.buisson@gmail.com>
2018-02-19 14:44:20 +0100

    Use clj-momo 0.2.20 stable

--- project.clj
+++ project.clj
@@ -48,7 +48,7 @@
                   :exclusions [threatgrid/flanders
                                metosin/ring-swagger
                                com.google.guava/guava]]
-                 [threatgrid/clj-momo "0.2.20-SNAPSHOT"]
+                 [threatgrid/clj-momo "0.2.20"]
 
                  ;; Web server
                  [metosin/compojure-api ~compojure-api-version
