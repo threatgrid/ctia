c1aeb9f89b8b80425f99d4fbb212b3f02b4a2c89
Guillaume Buisson <guillaume.buisson@gmail.com>
2018-05-04 10:03:24 +0200

    New Judgement Update route restricted to developer capabilities (#701)

--- src/ctia/entity/judgement/schemas.clj
+++ src/ctia/entity/judgement/schemas.clj
@@ -2,7 +2,7 @@
   (:require [clj-momo.lib.time :as time]
             [ctia.domain
              [access-control :refer [properties-default-tlp]]
-             [entities :refer [schema-version]]]
+             [entities :refer [schema-version make-valid-time]]]
             [ctia.schemas
              [core :refer [def-acl-schema def-stored-schema TempIDs]]
              [sorting :as sorting]]
@@ -37,36 +37,41 @@
   "partial-stored-judgement")
 
 (s/defn realize-judgement :- StoredJudgement
-  [new-judgement :- NewJudgement
-   id :- s/Str
-   tempids :- (s/maybe TempIDs)
-   owner :- s/Str
-   groups :- [s/Str]]
-  (let [now (time/now)
-        disposition (try
-                      (determine-disposition-id new-judgement)
-                      (catch clojure.lang.ExceptionInfo _
-                        (throw
-                         (http-response/bad-request!
-                          {:error "Mismatching :dispostion and dispositon_name for judgement"
-                           :judgement new-judgement}))))
-        disposition_name (get disposition-map disposition)]
-    (assoc new-judgement
-           :id id
-           :type "judgement"
-           :disposition disposition
-           :disposition_name disposition_name
-           :owner owner
-           :groups groups
-           :created now
-           :tlp (:tlp new-judgement (properties-default-tlp))
-           :schema_version schema-version
-           :valid_time {:end_time (or (get-in new-judgement
-                                              [:valid_time :end_time])
-                                      time/default-expire-date)
-                        :start_time (or (get-in new-judgement
-                                                [:valid_time :start_time])
-                                        now)})))
+  ([new-judgement :- NewJudgement
+    id :- s/Str
+    tempids :- (s/maybe TempIDs)
+    owner :- s/Str
+    groups :- [s/Str]]
+   (realize-judgement new-judgement id tempids owner groups nil))
+  ([new-judgement :- NewJudgement
+    id :- s/Str
+    tempids :- (s/maybe TempIDs)
+    owner :- s/Str
+    groups :- [s/Str]
+    prev-object :- (s/maybe StoredJudgement)]
+   (let [now (time/now)
+         disposition (try
+                       (determine-disposition-id new-judgement)
+                       (catch clojure.lang.ExceptionInfo _
+                         (throw
+                          (http-response/bad-request!
+                           {:error "Mismatching :dispostion and dispositon_name for judgement"
+                            :judgement new-judgement}))))
+         disposition_name (get disposition-map disposition)]
+     (merge new-judgement
+            {:id id
+             :type "judgement"
+             :disposition disposition
+             :disposition_name disposition_name
+             :owner (or (:owner prev-object) owner)
+             :groups (or (:groups prev-object) groups)
+             :created (or (:created prev-object) now)
+             :tlp (:tlp new-judgement
+                        (:tlp prev-object (properties-default-tlp)))
+             :schema_version schema-version}
+            (make-valid-time (:valid_time prev-object)
+                             (:valid_time new-judgement)
+                             now)))))
 
 (def judgement-fields
   (concat sorting/base-entity-sort-fields
