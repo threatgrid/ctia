d2ebeb4d5172a82e08d6c4321a9f93b1286eab13
Guillaume Buisson <guillaume.buisson@gmail.com>
2018-06-06 19:23:13 +0200

    complete bundle export tests

--- test/ctia/http/routes/bundle_test.clj
+++ test/ctia/http/routes/bundle_test.clj
@@ -424,12 +424,28 @@
             (get "ctia/bundle/export"
                  :query-params {:ids [sighting-id-1
                                       sighting-id-2]}
+                 :headers {"Authorization" "45c1f5e3f05d0"}))
+           bundle-get-res-4
+           (:parsed-body
+            (get "ctia/bundle/export"
+                 :query-params {:ids [sighting-id-1
+                                      sighting-id-2]
+                                :include_related_entities false}
                  :headers {"Authorization" "45c1f5e3f05d0"}))]
        (is (= 1 (count (:sightings bundle-get-res-1))))
+       (is (= 2 (count (:relationships bundle-get-res-1))))
        (is (= 2 (count (:indicators bundle-get-res-1))))
 
        (is (= 1 (count (:sightings bundle-get-res-2))))
+       (is (= 400 (count (:relationships bundle-get-res-2))))
        (is (= 400 (count (:indicators bundle-get-res-2))))
 
        (is (= 2 (count (:sightings bundle-get-res-3))))
-       (is (= 402 (count (:indicators bundle-get-res-3))))))))
+       (is (= 402 (count (:relationships bundle-get-res-3))))
+       (is (= 402 (count (:indicators bundle-get-res-3))))
+
+       (is (= 2 (count (:sightings bundle-get-res-4))))
+       (is (nil? (:indicators bundle-get-res-4)))
+       (is (= 402 (count (:relationships bundle-get-res-4))))
+
+       ))))
