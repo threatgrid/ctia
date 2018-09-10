9e901aa61ebdb58ead45f30249466aa208ba12a2
Guillaume Buisson <guillaume.buisson@gmail.com>
2018-09-10 20:36:02 +0200

    CTIM 1.0.4 (#740)
    * Upgrade Vulnerability mapping, schemas and graphql
    
    * Bump to CTIM 1.0.4

--- project.clj
+++ project.clj
@@ -44,7 +44,7 @@
                   :exclusions [prismatic/plumbing
                                potemkin
                                com.andrewmcveigh/cljs-time]]
-                 [threatgrid/ctim "1.0.2"
+                 [threatgrid/ctim "1.0.4"
                   :exclusions [threatgrid/flanders
                                metosin/ring-swagger
                                com.google.guava/guava]]
