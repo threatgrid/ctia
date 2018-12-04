b03fe9ae64c591ed441c2ee666090cc6792d1ad1
Guillaume Buisson <guillaume.buisson@gmail.com>
2018-12-04 18:23:51 +0100

    bump to CTIM stable

--- project.clj
+++ project.clj
@@ -44,7 +44,7 @@
                   :exclusions [prismatic/plumbing
                                potemkin
                                com.andrewmcveigh/cljs-time]]
-                 [threatgrid/ctim "1.0.8-SNAPSHOT"
+                 [threatgrid/ctim "1.0.8"
                   :exclusions [threatgrid/flanders
                                metosin/ring-swagger
                                com.google.guava/guava]]
