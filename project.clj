781334b6568ccb86e78157a012bba9d8e88e7811
Guillaume Buisson <guillaume@gbuisson.com>
2019-02-07 00:17:35 +0100

    fix riemann interval config config key name

--- project.clj
+++ project.clj
@@ -48,7 +48,7 @@
                   :exclusions [threatgrid/flanders
                                metosin/ring-swagger
                                com.google.guava/guava]]
-                 [threatgrid/clj-momo "0.2.29"]
+                 [threatgrid/clj-momo "0.2.30-SNAPSHOT"]
 
                  ;; Web server
                  [metosin/compojure-api ~compojure-api-version
