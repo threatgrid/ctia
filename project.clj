412b12a66a8b7f9f87204e597db3837dd8e25595
jyoverma <jyoverma@cisco.com>
2019-12-05 03:15:43 -0800

    using CTIM version 1.0.14 (#863)
    Using CTIM version 1.0.14
    
    <a name="qa">[§](#qa)</a> QA
    ============================
    
    No QA is needed.
    
    <a name="release-notes">[§](#release-notes)</a> Release Notes
    =============================================================
    
    ```
    intern: CTIM update with a new indicator type
    ```

--- project.clj
+++ project.clj
@@ -44,7 +44,7 @@
                   :exclusions [prismatic/plumbing
                                potemkin
                                com.andrewmcveigh/cljs-time]]
-                 [threatgrid/ctim "1.0.11"
+                 [threatgrid/ctim "1.0.14"
                   :exclusions [threatgrid/flanders
                                metosin/ring-swagger
                                com.google.guava/guava
