49a60c202146bd0367eaed0730ac97aa97e41180
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-03-16 13:46:13 -0400

    use transducer over reduce for clarity

--- src/ctia/task/update_mapping.clj
+++ src/ctia/task/update_mapping.clj
@@ -1,33 +1,33 @@
 (ns ctia.task.update-mapping
   "Updates the _mapping on an ES index."
   (:require [clj-momo.lib.es.index :as es-index]
-            [clj-momo.lib.es.schemas :refer [ESConnState]]
+            [clj-momo.lib.es.schemas :as es-schema]
             [clojure.tools.logging :as log]
-            [ctia.init :refer [init-store-service! log-properties]]
-            [ctia.properties :refer [init!]]
-            [ctia.store :refer [stores]]
+            [ctia.init :as init]
+            [ctia.properties :as properties]
+            [ctia.store :as store]
             [schema.core :as s])
-  (:import clojure.lang.ExceptionInfo))
+  (:import [clojure.lang ExceptionInfo]))
 
 (s/defn update-mapping-store
   "Updates _mapping on store"
-  [{:keys [conn index] :as cs} :- ESConnState
+  [{:keys [conn index] :as cs} :- es-schema/ESConnState
    mappings :- s/Any]
   (es-index/update-mapping! conn index mappings))
 
 (defn update-mapping-stores
-  [stores]
-  (reduce (fn [state [k store]]
-            (log/infof "updating _mapping for store: %s" k)
-            (assoc state k (mapv (fn [{:keys [state]}]
-                                   (update-mapping-store state (get-in state [:config :mappings])))
-                                 store)))
-          {}
-          stores))
+  [stores-map]
+  (into {}
+        (map (fn [[k stores]]
+               (log/infof "updating _mapping for store: %s" k)
+               [k (mapv (fn [{:keys [state]}]
+                          (update-mapping-store state (get-in state [:config :mappings])))
+                        stores)]))
+        stores-map))
 
 (defn -main [& _args]
-  (init!)
-  (log-properties)
-  (init-store-service!)
-  (let [res (update-mapping-stores @stores)]
+  (properties/init!)
+  (init/log-properties)
+  (init/init-store-service!)
+  (let [res (update-mapping-stores @store/stores)]
     (log/info "Completed update-mapping task: " res)))
