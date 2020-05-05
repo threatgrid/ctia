6ce71229ee0bde37c6dafbeb78b32f9703554951
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-05-05 15:27:22 -0400

    allow `added` and `deleted` fields in update change event

--- src/ctia/entity/event/obj_to_event.clj
+++ src/ctia/entity/event/obj_to_event.clj
@@ -2,6 +2,7 @@
   (:require [ctia.entity.event.schemas :as vs]
             [ctia.domain.entities :refer [with-long-id]]
             [clojure.data :refer [diff]]
+            [clojure.set :as set]
             [clj-momo.lib.time :as time]
             [clj-momo.lib.clj-time.core :as t]
             [schema.core :as s]))
@@ -18,8 +19,10 @@
    :tlp (:tlp object)
    :event_type vs/CreateEventType})
 
-(s/defn diff-to-list-of-changes :- [vs/Update]
-  "Given the output of a `diff` between maps return a list
+(s/defn ^:deprecated diff-to-list-of-changes :- [vs/Update]
+  "Deprecated in favor of `update-changes`.
+
+  Given the output of a `diff` between maps return a list
   of edit distance operation under the form of an Update map"
   [[diff-after diff-before _]]
   (filter
@@ -43,12 +46,49 @@
          (remove #(contains? diff-before %)
                  (keys diff-after))))))
 
+(defn update-changes
+  "Returns a list of changes for an Update event"
+  [old new]
+  {:pre [(map? old)
+         (map? new)]
+   :post [(vector? %)]}
+  (let [old-keys (set (keys old))
+        new-keys (set (keys new))
+        same-keys (set/intersection new-keys old-keys)
+        added-keys (set/difference new-keys old-keys)
+        deleted-keys (set/difference old-keys new-keys)]
+    (vec
+      ;; sort by :field then :action
+      (sort-by
+        (juxt :field :action)
+        (concat
+          (mapcat (fn [k]
+                    {:pre [(keyword? k)]}
+                    (let [[oldv newv] (map k [old new])]
+                      (when (not= oldv newv)
+                        [{:field k
+                          :action "modified"
+                          :change {:before oldv
+                                   :after newv}}])))
+                  same-keys)
+          (map (fn [k]
+                 {:pre [(keyword? k)]}
+                 {:field k
+                  :action "added"
+                  :change {:after (k new)}})
+               added-keys)
+          (map (fn [k]
+                 {:pre [(keyword? k)]}
+                 {:field k
+                  :action "deleted"
+                  :change {:before (k old)}})
+               deleted-keys))))))
 
 (s/defn to-update-event :- vs/Event
-  "transform an object (generally a `StoredObject`) to an `UpdateEvent`.
+  "Transform an object (generally a `StoredObject`) to an `UpdateEvent`.
    The two arguments `object` and `prev-object` should have the same schema.
-   The fields should contains enough information to retrieve all informations.
-   But the complete object is given for simplicity."
+   The fields should contain enough information to retrieve all information,
+   but the complete object is given for simplicity."
   [object prev-object event-id]
   {:owner (:owner object)
    :groups (:groups object)
@@ -58,10 +98,9 @@
    :type "event"
    :tlp (:tlp object)
    :event_type vs/UpdateEventType
-   :fields (diff-to-list-of-changes
-            (diff object (assoc
-                          prev-object
-                          :id (:id object))))})
+   :fields (update-changes
+             (dissoc prev-object :id)
+             (dissoc object :id))})
 
 (s/defn to-delete-event :- vs/Event
   "transform an object (generally a `StoredObject`) to its corresponding `Event`"
--- test/ctia/events_test.clj
+++ test/ctia/events_test.clj
@@ -6,6 +6,7 @@
                          use-fixtures
                          join-fixtures]]
    [clojure.core.async :refer [<!! chan poll! tap]]
+   [clojure.set :as set]
    [ctia.entity.event.obj-to-event :as o2e]
    [ctia.events :as e]
    [ctia.lib.async :as la]
@@ -75,3 +76,53 @@
     (is (= 1 (-> (<!! output) :entity :data)))
     (is (= 2 (-> (<!! output) :entity :data)))
     (is (nil? (poll! output)))))
+
+(deftest to-update-event-test
+  (let [old {:owner "tester"
+             :id "test-2"
+             :tlp "white"
+             :type :test
+             :data 2}]
+    (is (= [{:field :new, :action "added", :change {:after "bar"}}]
+           (:fields
+             (o2e/to-update-event
+               (assoc old :new "bar") old
+               "foo"))))
+    (is (= [{:field :data, :action "deleted", :change {:before 2}}]
+           (:fields
+             (o2e/to-update-event
+               (dissoc old :data) old
+               "foo"))))
+    (is (= [{:field :data, :action "modified",
+             :change {:before 2
+                      :after 3}}]
+           (:fields
+             (o2e/to-update-event
+               (assoc old :data 3) old
+               "foo"))))
+    (is (= [{:field :data,
+             :action "modified",
+             :change {:before [1], :after [1 2]}}]
+           (:fields
+             (o2e/to-update-event
+               (assoc old :data [1 2])
+               (assoc old :data [1])
+               "foo"))))
+    (is (= [{:action "added"
+             :field :added
+             :change {:after 1}}
+            {:action "modified"
+             :field :data
+             :change {:before {}
+                      :after {:a 1}}}
+            {:action "deleted"
+             :field :removed
+             :change {:before 1}}]
+           (:fields
+             (o2e/to-update-event
+               (-> old
+                   (assoc :data {:a 1}
+                          :added 1)
+                   (dissoc :removed))
+               (assoc old :data {} :removed 1)
+               "foo"))))))
