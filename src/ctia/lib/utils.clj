62cd57fb6e66f65042c0404d2dd21a769e9cf60d
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-07-09 17:48:00 -0400

    replace obfuscate functions with iroh's

--- src/ctia/lib/utils.clj
+++ src/ctia/lib/utils.clj
@@ -3,15 +3,63 @@
             [clojure.tools.logging :as log]
             [clojure.walk :as walk]))
 
-(defn filter-out-creds [m]
+;; copied from log-helper.safe
+(def to-obfuscate-pattern
+  (let [keywords-to-obfuscate ["authorization"
+                               "creds"
+                               "key"
+                               "basic-auth"
+                               "pass"
+                               "secret"
+                               "tempfile"
+                               "customerKey"
+                               "response-url"
+                               "token"]]
+    (re-pattern (str "(?i).*("
+                     (string/join "|" keywords-to-obfuscate)
+                     ").*"))))
+
+;; copied from log-helper.safe
+(def to-obfuscate-keys
+  #{"jwt" "authorization"})
+
+;; copied from log-helper.safe
+(defn obfuscate?
+  [k v]
+  (let [k-str (if (or (keyword? k) (string? k))
+                (string/lower-case (name k))
+                (do
+                  ;; SHOULD not occurs except during development
+                  ;; only a warn because it is a bug that do not affect the
+                  ;; end-user.
+                  (log/warnf "Some hash-map with key not a keyword nor string: %s."
+                             (clojure.core/pr-str k))
+                  (clojure.core/pr-str k)))]
+    (cond
+      (contains? to-obfuscate-keys k-str) (string? v)
+      :else (some? (re-matches to-obfuscate-pattern k-str)))))
+
+;; copied from log-helper.safe
+(defn obfuscate-nested-map
+  "Obfuscate a value recursively by keeping the structure and the type"
+  [v]
+  (walk/prewalk #(if (string? %) "********" %) v))
+
+;; copied from log-helper.safe
+(defn filter-out-creds
+  "Given an hash-map obfuscate credentials."
+  [m]
   (reduce-kv (fn [acc k v]
-               (if (re-matches #"(?i).*(key|pass|token|secret).*" (str k))
-                 (assoc acc k "********")
-                 (assoc acc k v)))
+               (cond
+                 (obfuscate? k v) (update acc k obfuscate-nested-map)
+                 :else (assoc acc k v)))
              m
              m))
 
-(defn deep-filter-out-creds [m]
+;; copied from log-helper.safe
+(defn deep-filter-out-creds
+  "Given a structure walk deeply into it to obfuscate any found credential."
+  [m]
   (walk/prewalk #(if (map? %)
                    (filter-out-creds %)
                    %)
@@ -41,6 +89,30 @@
       :else cleaned)))
 
 ;; copied from iroh-core.core
+(defn map-filter
+  "filter generalized to maps"
+  [f m]
+  (into {}
+        (for [[k v] m :when (f v)] [k v])))
+
+;; copied from iroh-core.core
+(defn clean-map
+  "Remove map entries whose value is not a map, satisfy the `f` predicate
+   or is equal to ::to-remove. If the resulting map is empty `::to-remove`
+   is returned."
+  [f m]
+  (let [cleaned (map-filter
+                 (fn [x]
+                   (if (map? x)
+                     true
+                     (and (not= ::to-remove x) (f x))))
+                 m)]
+    (if (and (seq m)
+             (empty? cleaned))
+      ::to-remove
+      cleaned)))
+
+;; copied from iroh-core.core
 (defn deep-filter
   "Deeply nested filter. Filter on leaves only and remove the un-needed subtrees."
   [f m]
@@ -60,43 +132,3 @@
 (def deep-remove-nils
   "Remove nil values from a deep nested map recursively"
   (partial deep-filter some?))
-
-;; copied from log-helper.safe
-(def to-obfuscate-keys
-  #{"jwt" "authorization"})
-
-;; copied from log-helper.safe
-(defn obfuscate?
-  [k v]
-  (let [k-str (if (or (keyword? k) (string? k))
-                (string/lower-case (name k))
-                (do
-                  ;; SHOULD not occurs except during development
-                  ;; only a warn because it is a bug that do not affect the
-                  ;; end-user.
-                  (log/warnf "Some hash-map with key not a keyword nor string: %s."
-                             (clojure.core/pr-str k))
-                  (clojure.core/pr-str k)))]
-    (cond
-      (contains? to-obfuscate-keys k-str) (string? v)
-      :else (some? (re-matches to-obfuscate-pattern k-str)))))
-
-;; copied from log-helper.safe
-(defn- filter-out-creds
-  "Given an hash-map obfuscate credentials."
-  [m]
-  (reduce-kv (fn [acc k v]
-               (cond
-                 (obfuscate? k v) (update acc k obfuscate-nested-map)
-                 :else (assoc acc k v)))
-             m
-             m))
-
-;; copied from log-helper.safe
-(defn deep-filter-out-creds
-  "Given a structure walk deeply into it to obfuscate any found credential."
-  [m]
-  (walk/prewalk #(if (map? %)
-                   (filter-out-creds %)
-                   %)
-                m))
