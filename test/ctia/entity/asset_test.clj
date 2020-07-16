4473f4d0607dcf9cc71cdde4e18cf116034f41c9
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-07-15 17:52:20 -0700

    Asset pagination test

--- test/ctia/entity/asset_test.clj
+++ test/ctia/entity/asset_test.clj
@@ -1,13 +1,16 @@
 (ns ctia.entity.asset-test
   (:require [clj-momo.test-helpers.core :as mth]
             [clojure.test :refer [deftest is are join-fixtures testing use-fixtures]]
+            [ctia.entity.asset :as asset]
             [ctia.test-helpers.auth :as auth]
             [ctia.test-helpers.core :as helpers]
             [ctia.test-helpers.crud :refer [entity-crud-test]]
             [ctia.test-helpers.fake-whoami-service :as whoami-helpers]
+            [ctia.test-helpers.field-selection :as field-selection]
             [ctia.test-helpers.http :as http]
-            [ctia.test-helpers.store :refer [test-for-each-store]]
-            [ctim.examples.assets :refer [new-asset-maximal]]))
+            [ctia.test-helpers.pagination :as pagination]
+            [ctia.test-helpers.store :as store]
+            [ctim.examples.assets :refer [new-asset-minimal new-asset-maximal]]))
 
 (use-fixtures :once (join-fixtures [mth/fixture-schema-validation
                                     helpers/fixture-properties:clean
@@ -35,7 +38,7 @@
         ))))
 
 (deftest asset-routes-test
-  (test-for-each-store
+  (store/test-for-each-store
    (fn []
      (helpers/set-capabilities! "foouser" ["foogroup"] "user" auth/all-capabilities)
      (whoami-helpers/set-whoami-response http/api-key "foouser" "foogroup" "user")
@@ -48,4 +51,30 @@
        :update-field     :source
        :additional-tests additional-tests
        :headers          {:Authorization "45c1f5e3f05d0"}}))))
+
+(deftest asset-pagination-test
+  (store/test-for-each-store
+   (fn []
+     (helpers/set-capabilities! "foouser" ["foogroup"] "user" auth/all-capabilities)
+     (whoami-helpers/set-whoami-response "45c1f5e3f05d0"
+                                         "foouser"
+                                         "foogroup"
+                                         "user")
+
+     (let [ids (helpers/post-entity-bulk
+                new-asset-maximal
+                :assets
+                30
+                {"Authorization" "45c1f5e3f05d0"})]
+
+       (field-selection/field-selection-tests
+        ["ctia/asset/search?query=*"
+         (http/doc-id->rel-url (first ids))]
+        {"Authorization" "45c1f5e3f05d0"}
+        asset/asset-fields)
+
+       (pagination/pagination-test
+        "ctia/asset/search?query=*"
+        {"Authorization" "45c1f5e3f05d0"}
+        asset/asset-fields)))))
 
