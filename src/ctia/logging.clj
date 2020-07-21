db492c31d014888f55619d3ac8303569523b181d
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-07-21 17:37:23 -0400

    logging fixes

--- src/ctia/logging.clj
+++ src/ctia/logging.clj
@@ -1,11 +1,6 @@
 (ns ctia.logging
-  (:require [ctia.logging-core]))
-
-(defn init!
-  "Sets up logging of all events"
-  []
-  (let [control (e/register-listener #(log/info "event:" %) :blocking)]
-    (shutdown/register-hook! :logging #(a/close! control))))
+  (:require [ctia.logging-core :as core]
+            [puppetlabs.trapperkeeper.core :as tk]))
 
 (tk/defservice event-logging-service
   [[:EventsService register-listener]]
