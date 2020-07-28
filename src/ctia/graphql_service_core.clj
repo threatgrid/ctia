44363f3edee73a556f5498aa0b9416e4e5763e52
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-07-28 09:52:11 -0400

    doc

--- src/ctia/graphql_service_core.clj
+++ src/ctia/graphql_service_core.clj
@@ -1,23 +1,30 @@
 (ns ctia.graphql-service-core
   (:require [puppetlabs.trapperkeeper.core :as tk]
             [ctia.graphql.schemas :as schemas]
-            [ctia.schemas.graphql.helpers :as helpers]))
+            [ctia.schemas.graphql.helpers :as helpers])
+  (:import [graphql GraphQL]))
 
+;; :graphql-promise is a Promise<GraphQL>
 (defn get-graphql [{:keys [graphql-promise]}]
+  {:post [(instance? GraphQL %)]}
   @graphql-promise)
 
+;; :type-registry is an Atom<{String, Promise<graphql.*>}>
 (defn get-or-update-type-registry [type-registry name f]
-  (or (some-> (get @type-registry name) deref)
+  {:post [%]}
+  (or ;; fast-path for readers
+      (some-> (get @type-registry name) deref)
+      ;; might need to generate a value (or coordinate with another thread doing so)
       (let [[{oldprm name} {newprm name}] (swap-vals! type-registry
                                                       (fn [{oldprm name :as oldtr}]
                                                         (cond-> oldtr
                                                           (not oldprm)
                                                           (assoc name (promise)))))
             _ (assert (or oldprm newprm))]
-        (when (not= oldprm newprm)
-          (do (assert (not oldprm))
-              (assert newprm)
-              (deliver newprm (f))))
+        ;; if oldprm is nil, we're in charge of delivering newprm.
+        ;; otherwise, another thread will deliver newprm.
+        (when (nil? oldprm)
+          (deliver newprm (f)))
         @newprm)))
 
 (defn init [context]
