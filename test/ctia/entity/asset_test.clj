bec6030edcf8db7b413e7bbe26b4a71b6d6f44c1
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-07-31 15:08:59 -0700

    adjust test to the latest changes
    after rebasing on current 'master', the test broke due to changes in:
    https://github.com/threatgrid/ctia/pull/933

--- test/ctia/entity/asset_test.clj
+++ test/ctia/entity/asset_test.clj
@@ -2,6 +2,7 @@
   (:require [clj-momo.test-helpers.core :as mth]
             [clojure.test :refer [deftest is are join-fixtures testing use-fixtures]]
             [ctia.entity.asset :as asset]
+            [ctia.properties :as p]
             [ctia.test-helpers.aggregate :as aggregate]
             [ctia.test-helpers.auth :as auth]
             [ctia.test-helpers.core :as helpers]
@@ -22,7 +23,7 @@
 (defn additional-tests [asset-id asset-sample]
   (testing "GET /ctia/asset/search"
     ;; only when ES store
-    (when (= "es" (get-in @ctia.properties/properties [:ctia :store :asset]))
+    (when (= "es" (p/get-in-global-properties [:ctia :store :asset]))
       (are [term check-fn expected desc] (let [response (helpers/get (str "ctia/asset/search")
                                                                      :query-params {"query" term}
                                                                      :headers {"Authorization" "45c1f5e3f05d0"})]
