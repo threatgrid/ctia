49c60b89596550a9ae42c57c0026dce86121d198
Guillaume Erétéo <gereteo@cisco.com>
2020-08-05 13:07:09 +0200

    Ambigous index names 4012 (#934)
    > Related #https://github.com/threatgrid/iroh/issues/4012 -
    
    This PR prevents CTIA to start with ambiguous index names like `ctia_feed` and `ctia_feedback` (we use `ctia_feeds` in production). It also turns on the Sytem/exit on invalid mappings that was waiting the migration of the vulnerability index that is now done in all environment.

--- test/ctia/stores/es/init_test.clj
+++ test/ctia/stores/es/init_test.clj
@@ -87,6 +87,56 @@
       (is (= "2s" refresh_interval)))
     (index/delete! conn (str indexname "*"))))
 
+(deftest get-existing-indices-test
+  (index/delete! es-conn (str indexname "*"))
+  (let [successful? (atom true)
+        fake-exit (fn [] (reset! successful? false))]
+    (let [test-fn (fn [msg
+                       input-indexname
+                       expected-successful?
+                       expected-output]
+                    (assert (boolean? expected-successful?))
+                    (reset! successful? true)
+                    (testing msg
+                      (let [ouput (with-redefs [sut/system-exit-error fake-exit]
+                                    (sut/get-existing-indices es-conn input-indexname))]
+                        ;; sut/system-exit-error is redefined to check if
+                        ;; it was called but avoid to actually System/exit
+                        ;; thus testing the output makes sense only on success
+                        (when expected-successful?
+                          (is (= expected-output ouput)))
+                        (is (= expected-successful? @successful?)))))
+
+          _ (test-fn "0 existing index"
+                     indexname
+                     true
+                     #{})
+
+          _ (index/create! es-conn indexname {})
+          _ (test-fn "1 existing index with the exact name"
+                     indexname
+                     true
+                     #{(keyword indexname)})
+
+          indexname-with-date (str indexname "-2020.07.31")
+          _ (index/create! es-conn indexname-with-date {})
+          _ (test-fn "2 existing indices, 1 with exact name, 1 suffixed with date"
+                     indexname
+                     true
+                     #{(keyword indexname-with-date)
+                       (keyword indexname)})
+
+          ;; generate an indexname which is a prefix of indexname
+          ambiguous-indexname (->> (count indexname)
+                                   dec
+                                   (subs indexname 0))]
+      (test-fn "CTIA must fail to start with configuration having ambiguous index names between stores"
+               ambiguous-indexname
+               false
+               nil)
+      ;; clean
+      (index/delete! es-conn (str indexname "*")))))
+
 (deftest init-es-conn!-test
   (index/delete! es-conn (str indexname "*"))
   (testing "init-es-conn! should return a proper conn state with unaliased conf, but not create any index"
@@ -104,9 +154,9 @@
       (is (= {} (select-keys (:mappings config) [:a :b])))))
 
   (testing "update mapping should allow adding fields or identical mapping"
-    (let [exited (atom false)
-          fake-exit (fn [] (reset! exited true))
-          test-fn (fn [msg error? field field-mapping]
+    (let [sucessful? (atom true)
+          fake-exit (fn [] (reset! sucessful? false))
+          test-fn (fn [msg expected-successful? field field-mapping]
                     ;; init and create aliased indices
                     (sut/init-es-conn! props-aliased)
                     (with-redefs [sut/system-exit-error fake-exit
@@ -115,19 +165,21 @@
                                   (cond-> sut/store-mappings
                                     field (assoc-in [:sighting "sighting" :properties field]
                                                     field-mapping))]
-                      ;; init again to trigger mapping update
-                      (sut/init-es-conn! props-aliased)
-                      ; check state
-                      (is (= error? @exited) msg)
+                      (testing msg
+                        ;; init again to trigger mapping update
+                        (sut/init-es-conn! props-aliased)
+                        ;; check state
+                        (is (= expected-successful? @sucessful?)))
                       ;; reset state
                       (index/delete! es-conn (str indexname "*"))
-                      (reset! exited false)))]
+                      (http/delete (str "http://localhost:9200/_template/" indexname "*"))
+                      (reset! sucessful? true)))]
       (test-fn "update mapping should not fail on unchanged mapping"
-               false nil nil)
+               true nil nil)
       (test-fn "update mapping should not fail on field addition"
-               false :new-field m/token)
-      (test-fn "update mapping should fail when modifying existing field mapping"
-               true :id m/text)))
+               true :new-field m/token)
+      (test-fn "Update mapping fails when modifying existing field mapping and CTIA must not start in that case."
+               false :id m/text)))
 
   (testing "init-es-conn! should return a proper conn state with aliased conf, and create an initial aliased index"
     (index/delete! es-conn (str indexname "*"))
@@ -176,6 +228,6 @@
       (is (= 1 (get-in config [:settings :number_of_replicas])))
       (is (= 2 (get-in config [:settings :number_of_shards])))
       (is (= {} (select-keys (:mappings config) [:a :b])))))
-
+  ;; clean
   (http/delete (str "http://localhost:9200/_template/" indexname "*"))
   (index/delete! es-conn (str indexname "*")))
