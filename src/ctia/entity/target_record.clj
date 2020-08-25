66eedb147277eef7e0199d1310f9c0523ec163b8
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-08-25 15:18:01 -0700

    formatting

--- src/ctia/entity/target_record.clj
+++ src/ctia/entity/target_record.clj
@@ -121,8 +121,7 @@
     :external-id-capabilities :read-target-record
     :can-aggregate?           true
     :histogram-fields         target-record-histogram-fields
-    :enumerable-fields        target-record-enumerable-fields
-    }))
+    :enumerable-fields        target-record-enumerable-fields}))
 
 (def capabilities
   #{:create-target-record
