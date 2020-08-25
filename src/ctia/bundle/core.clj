e565599b2ffd1594386d8fc930da9ff3bcea9d43
Guillaume Erétéo <gereteo@cisco.com>
2020-08-25 09:11:58 +0200

    Fix bundle import OOMs (#947)
    We previously noticed an increase in CTIA memory usage which did not cause any problem until recent OOM in PROD. The analysing of the hprofs memory dump shows that the OOM happens in the `ctia.core.bundle/find-by-external-ids` function.
    Up to million of duplicated entities were already created. However, during the deduplication process, the `bundle/import` retrieves in the `all-pages` function (called by `find-by-external-ids`) all the entities that match a list of external ids, including all the duplicates which end loading a huge amount of entities in memory.
    It is very likely that it could be the cause of the observed OOM.
    This PR optimizes the `all-pages` function by querying the subsequent pages with only the `external_-ids` that were not matched yet.

--- src/ctia/bundle/core.clj
+++ src/ctia/bundle/core.clj
@@ -22,7 +22,7 @@
    [ctim.domain.id :as id]
    [schema.core :as s]))
 
-(def find-by-external-ids-limit 1000)
+(def find-by-external-ids-limit 200)
 
 (def bundle-entity-keys
   (set (vals bulk/bulk-entity-mapping)))
@@ -77,15 +77,22 @@
 
 (defn all-pages
   "Retrieves all external ids using pagination."
-  [f]
-  (loop [paging {:offset 0
-                 :limit find-by-external-ids-limit}
+  [entity-type external-ids auth-identity]
+  (loop [ext-ids external-ids
          entities []]
-    (let [{results :data
-           {next-page :next} :paging} (f paging)
-          acc-entities (into entities results)]
+    (let [query {:all-of {:external_ids ext-ids}}
+          paging {:limit find-by-external-ids-limit}
+          {results :data
+           {next-page :next} :paging} (read-store entity-type
+                                                  list-fn
+                                                  query
+                                                  (auth/ident->map auth-identity)
+                                                  paging)
+          acc-entities (into entities results)
+          matched-ext-ids (into #{} (mapcat :external_ids results))
+          remaining-ext-ids (remove matched-ext-ids ext-ids)]
       (if next-page
-        (recur next-page acc-entities)
+        (recur remaining-ext-ids acc-entities)
         acc-entities))))
 
 (defn find-by-external-ids
@@ -96,12 +103,7 @@
                 (pr-str external-ids))
     (if (seq external-ids)
       (debug (format "Results for %s:" (pr-str external-ids))
-             (all-pages
-              (fn [paging]
-                (read-store entity-type list-fn
-                            {:all-of {:external_ids external-ids}}
-                            (auth/ident->map auth-identity)
-                            paging))))
+             (all-pages entity-type external-ids auth-identity))
       [])))
 
 (defn by-external-id
@@ -156,8 +158,8 @@
    an error is reported."
   [{:keys [external_ids]
     :as entity-data} :- EntityImportData
-   find-by-external-ids]
-  (if-let [old-entities (mapcat find-by-external-ids external_ids)]
+   find-by-external-id]
+  (if-let [old-entities (mapcat find-by-external-id external_ids)]
     (let [old-entity (some-> old-entities
                              first
                              :entity
