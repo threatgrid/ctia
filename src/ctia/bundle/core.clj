threatgrid/ctia

commit 1b12ca632da955a4a0df4013b9e48110f6bb6aea

TK2.5: add service args to ctia.bundle.core (#954)
<!-- Specify linked issues and REMOVE THE UNUSED LINES -->

Epic: threatgrid/iroh#3986
Extracted from https://github.com/threatgrid/ctia/pull/924

Note: since bundle uses bulk, there's some overlap here with https://github.com/threatgrid/ctia/pull/950 and will require a merge.


<!--

Describe your PR for reviewers.
Don't forget to set correct labels (User Facing / Beta / Feature Flag)
If there is UI change please add a screen capture.
-->

<!-- UNCOMMENT THIS SECTION IF NEEDED
<a name="iroh-services-clients">[§](#iroh-services-clients)</a> IROH Services Clients
=====================================================================================

Put all informations that need to be communicated to IROH Services Clients.
Typically IROH-UI, ATS Integration, Orbital, etc...
 -->

<a name="qa">[§](#qa)</a> QA
============================

<!--
Describe the steps to test your PR.

1.
2.
3.

Or if no QA is needed keep it as is.
 -->
No QA is needed.

<!-- UNCOMMENT THIS SECTION IF NEEDED
<a name="ops">[§](#ops)</a> Ops
===============================

  Specify Ops related issues and documentation
- Config change needed: threatgrid/tenzin#
- Migration needed: threatgrid/tenzin#
- How to enable/disable that feature: (ex remove service from `bootstrap.cfg`, add scope to org)
-->

<!-- UNCOMMENT THIS SECTION IF NEEDED
<a name="documentation">[§](#documentation)</a> Documentation
=============================================================

  Public Facing documentation section;
- Public documentation updated needed: threatgrid/iroh-ui#
  See internal [doc file](./services/iroh-auth/doc/public-doc.org)
 -->

<a name="release-notes">[§](#release-notes)</a> Release Notes
=============================================================

<!-- REMOVE UNUSED LINES -->

```
intern: TK2.5: add service args to ctia.bundle.core
```

src/ctia/bundle/core.clj
@@ -10,15 +10,14 @@
     [auth :as auth]
     [properties :as p]
     [store :refer [list-fn
-                   read-fn
-                   read-store]]]
+                   read-fn]]]
    [ctia.lib.collection :as coll :refer [fmap]]
    [ctia.bulk.core :as bulk]
    [ctia.bundle.schemas
     :refer
     [BundleImportData BundleImportResult EntityImportData]]
    [ctia.domain.entities :as ent :refer [with-long-id]]
-   [ctia.schemas.core :refer [NewBundle TempIDs]]
+   [ctia.schemas.core :refer [APIHandlerServices NewBundle TempIDs]]
    [ctim.domain.id :as id]
    [schema.core :as s]))
 
@@ -95,8 +94,10 @@
         (recur remaining-ext-ids acc-entities)
         acc-entities))))
 
-(defn find-by-external-ids
-  [import-data entity-type auth-identity]
+(s/defn find-by-external-ids
+  [import-data entity-type auth-identity
+   {{:keys [read-store]} :StoreService
+    :as _services_} :- APIHandlerServices]
   (let [external-ids (mapcat :external_ids import-data)]
     (log/debugf "Searching %s matching these external_ids %s"
                 entity-type
@@ -186,12 +187,14 @@
 
 (s/defn with-existing-entities :- [EntityImportData]
   "Add existing entities to the import data map."
-  [import-data entity-type identity-map]
+  [import-data entity-type identity-map
+   services :- APIHandlerServices]
   (let [entities-by-external-id
         (by-external-id
          (find-by-external-ids import-data
                                entity-type
-                               identity-map))
+                               identity-map
+                               services))
         find-by-external-id-fn (fn [external_id]
                                  (when external_id
                                    (get entities-by-external-id
@@ -205,12 +208,13 @@
    will be imported"
   [bundle-entities
    external-key-prefixes
-   auth-identity]
+   auth-identity
+   services :- APIHandlerServices]
   (map-kv (fn [k v]
             (let [entity-type (bulk/entity-type-from-bulk-key k)]
               (-> v
                   (init-import-data entity-type external-key-prefixes)
-                  (with-existing-entities entity-type auth-identity))))
+                  (with-existing-entities entity-type auth-identity services))))
           bundle-entities))
 
 (defn create?
@@ -258,9 +262,9 @@
              #(dissoc % :new-entity :old-entity)
              (apply concat (vals bundle-import-data)))})
 
-(defn bulk-params []
+(defn bulk-params [get-in-config]
   {:refresh
-   (p/get-in-global-properties [:ctia :store :bundle-refresh] "false")})
+   (get-in-config [:ctia :store :bundle-refresh] "false")})
 
 (defn log-errors
   [response]
@@ -274,18 +278,21 @@
 (s/defn import-bundle :- BundleImportResult
   [bundle :- NewBundle
    external-key-prefixes :- (s/maybe s/Str)
-   auth-identity :- (s/protocol auth/IIdentity)]
+   auth-identity :- (s/protocol auth/IIdentity)
+   {{:keys [get-in-config]} :ConfigService
+    :as services} :- APIHandlerServices]
   (let [bundle-entities (select-keys bundle bundle-entity-keys)
         bundle-import-data (prepare-import bundle-entities
                                            external-key-prefixes
-                                           auth-identity)
+                                           auth-identity
+                                           services)
         bulk (debug "Bulk" (prepare-bulk bundle-import-data))
         tempids (->> bundle-import-data
                      (map (fn [[_ entities-import-data]]
                             (entities-import-data->tempids entities-import-data)))
                      (apply merge {}))]
     (debug "Import bundle response"
-           (->> (bulk/create-bulk bulk tempids auth-identity (bulk-params))
+           (->> (bulk/create-bulk bulk tempids auth-identity (bulk-params get-in-config))
                 (with-bulk-result bundle-import-data)
                 build-response
                 log-errors))))
@@ -301,11 +308,11 @@
 (defn local-entity?
   "Returns true if this entity'ID is hosted by this CTIA instance,
    false otherwise"
-  [id]
+  [id get-in-config]
   (if (seq id)
     (if (id/long-id? id)
       (let [id-rec (id/long-id->id id)
-            this-host (p/get-in-global-properties [:ctia :http :show :hostname])]
+            this-host (get-in-config [:ctia :http :show :hostname])]
         (= (:hostname id-rec) this-host))
       true)
     false))
@@ -316,15 +323,17 @@
        (filter (comp seq second))
        (into {})))
 
-(defn fetch-relationship-targets
+(s/defn fetch-relationship-targets
   "given relationships, fetch all related objects"
-  [relationships identity-map]
+  [relationships identity-map
+   {{:keys [get-in-config]} :ConfigService
+    :as services} :- APIHandlerServices]
   (let [all-ids (->> relationships
                      (map (fn [{:keys [target_ref source_ref]}]
                             [target_ref source_ref]))
                      flatten
                      set
-                     (filter local-entity?)
+                     (filter #(local-entity? % get-in-config))
                      set)
         by-type (dissoc (group-by
                          #(ent/long-id->entity-type %) all-ids) nil)
@@ -351,13 +360,16 @@
           (when (seq node-filters)
             {:query node-filters}))))
 
-(defn fetch-entity-relationships
+(s/defn fetch-entity-relationships
   "given an entity id, fetch all related relationship"
   [id
    identity-map
-   filters]
+   filters
+   {{:keys [get-in-config]} :ConfigService
+    {:keys [read-store]} :StoreService
+    :as _services_} :- APIHandlerServices]
   (let [filter-map (relationships-filters id filters)
-        max-relationships (p/get-in-global-properties [:ctia :http :bundle :export :max-relationships]
+        max-relationships (get-in-config [:ctia :http :bundle :export :max-relationships]
                                                       1000)]
     (some-> (:data (read-store :relationship
                                list-fn
@@ -368,9 +380,11 @@
                                 :sort_order "desc"}))
             ent/un-store-all)))
 
-(defn fetch-record
+(s/defn fetch-record
   "Fetch a record by ID guessing its type"
-  [id identity-map]
+  [id identity-map
+   {{:keys [read-store]} :StoreService
+    :as _services_} :- APIHandlerServices]
   (when-let [entity-type (ent/id->entity-type id)]
     (read-store (keyword entity-type)
                 read-fn
@@ -378,16 +392,17 @@
                 identity-map
                 {})))
 
-(defn export-entities
+(s/defn export-entities
   "Given an entity id, export it along
    with its relationship as a Bundle"
   [id
    identity-map
    ident
-   params]
-  (if-let [record (fetch-record id identity-map)]
+   params
+   services :- APIHandlerServices]
+  (if-let [record (fetch-record id identity-map services)]
     (let [relationships (when (:include_related_entities params true)
-                          (fetch-entity-relationships id identity-map params))]
+                          (fetch-entity-relationships id identity-map params services))]
       (cond-> {}
         record
         (assoc (-> (:type record)
@@ -405,18 +420,20 @@
         (->> (deep-merge-with coll/add-colls
                               (fetch-relationship-targets
                                relationships
-                               ident)))))
+                               ident
+                               services)))))
     {}))
 
 (def empty-bundle
   {:type "bundle"
    :source "ctia"})
 
-(defn export-bundle
+(s/defn export-bundle
   [ids
    identity-map
    ident
-   params]
-  (->> (map #(export-entities % identity-map ident params) ids)
+   params
+   services :- APIHandlerServices]
+  (->> (map #(export-entities % identity-map ident params services) ids)
        (reduce #(deep-merge-with coll/add-colls %1 %2))
        (into empty-bundle)))

src/ctia/bundle/routes.clj
@@ -1,7 +1,7 @@
 (ns ctia.bundle.routes
   (:refer-clojure :exclude [identity])
   (:require
-   [compojure.api.sweet :refer [GET POST context defroutes describe]]
+   [compojure.api.sweet :refer [GET POST context routes describe]]
    [ctia.bundle
     [core :refer [bundle-max-size
                   bundle-size
@@ -12,7 +12,7 @@
                      BundleExportIds
                      BundleExportOptions
                      BundleExportQuery]]]
-   [ctia.schemas.core :refer [NewBundle]]
+   [ctia.schemas.core :refer [APIHandlerServices NewBundle]]
    [ring.util.http-response :refer [ok bad-request]]
    [schema.core :as s]
    [schema-tools.core :as st]))
@@ -63,7 +63,8 @@
     :read-casebook
     :list-casebooks})
 
-(defroutes bundle-routes
+(s/defn bundle-routes [services :- APIHandlerServices]
+ (routes 
   (context "/bundle" []
            :tags ["Bundle"]
            (GET "/export" []
@@ -77,7 +78,8 @@
                      (:ids q)
                      identity-map
                      identity
-                     q)))
+                     q
+                     services)))
 
            (POST "/export" []
                 :return NewBundleExport
@@ -91,7 +93,8 @@
                      (:ids b)
                      identity-map
                      identity
-                     q)))
+                     q
+                     services)))
 
            (POST "/import" []
                  :return BundleImportResult
@@ -125,4 +128,4 @@
                  (let [max-size (bundle-max-size)]
                    (if (< max-size (bundle-size bundle))
                      (bad-request (str "Bundle max nb of entities: " max-size))
-                     (ok (import-bundle bundle external-key-prefixes auth-identity)))))))
+                     (ok (import-bundle bundle external-key-prefixes auth-identity services))))))))

src/ctia/http/handler.clj
@@ -227,7 +227,7 @@
                    "/incident" []
                  :tags ["Incident"]
                  incident-link-route)
-               bundle-routes
+               (bundle-routes services)
                observable-routes
                metrics-routes
                properties-routes

test/ctia/bundle/core_test.clj
@@ -11,7 +11,7 @@
                   h/fixture-ctia-fast]))
 
 (deftest local-entity?-test
-  (are [x y] (= x (sut/local-entity? y))
+  (are [x y] (= x (sut/local-entity? y (h/current-get-in-config-fn)))
     false nil
     false "http://unknown.site/ctia/indicator/indicator-56067199-47c0-4294-8957-13d6b265bdc4"
     true "indicator-56067199-47c0-4294-8957-13d6b265bdc4"

commit 351c1d216482dfc05f1875b5a97e3464f32622a6

add service args to ctia.bundle.core/all-pages (#964)
Fixes a bad merge between https://github.com/threatgrid/ctia/commit/974e1c87ed0695d0db9a4a16e642dec883ec3a83 and https://github.com/threatgrid/ctia/commit/1b12ca632da955a4a0df4013b9e48110f6bb6aea caused by pressing the "squash and merge" button. Could have been avoided if I manually merged https://github.com/threatgrid/ctia/pull/954 with master, which would have made the PR CI job fail, but I had too much faith in the GitHub green tick.

<!-- Specify linked issues and REMOVE THE UNUSED LINES -->


<!--

Describe your PR for reviewers.
Don't forget to set correct labels (User Facing / Beta / Feature Flag)
If there is UI change please add a screen capture.
-->

<!-- UNCOMMENT THIS SECTION IF NEEDED
<a name="iroh-services-clients">[§](#iroh-services-clients)</a> IROH Services Clients
=====================================================================================

Put all informations that need to be communicated to IROH Services Clients.
Typically IROH-UI, ATS Integration, Orbital, etc...
 -->

<a name="qa">[§](#qa)</a> QA
============================

<!--
Describe the steps to test your PR.

1.
2.
3.

Or if no QA is needed keep it as is.
 -->
No QA is needed.

<!-- UNCOMMENT THIS SECTION IF NEEDED
<a name="ops">[§](#ops)</a> Ops
===============================

  Specify Ops related issues and documentation
- Config change needed: threatgrid/tenzin#
- Migration needed: threatgrid/tenzin#
- How to enable/disable that feature: (ex remove service from `bootstrap.cfg`, add scope to org)
-->

<!-- UNCOMMENT THIS SECTION IF NEEDED
<a name="documentation">[§](#documentation)</a> Documentation
=============================================================

  Public Facing documentation section;
- Public documentation updated needed: threatgrid/iroh-ui#
  See internal [doc file](./services/iroh-auth/doc/public-doc.org)
 -->

src/ctia/bundle/core.clj
@@ -74,9 +74,9 @@
       (transient-id? id) (assoc :original_id id)
       (seq filtered-ext-ids) (assoc :external_ids filtered-ext-ids))))
 
-(defn all-pages
+(s/defn all-pages
   "Retrieves all external ids using pagination."
-  [entity-type external-ids auth-identity]
+  [entity-type external-ids auth-identity read-store :- (s/pred ifn?)]
   (loop [ext-ids external-ids
          entities []]
     (let [query {:all-of {:external_ids ext-ids}}
@@ -97,14 +97,14 @@
 (s/defn find-by-external-ids
   [import-data entity-type auth-identity
    {{:keys [read-store]} :StoreService
-    :as _services_} :- APIHandlerServices]
+    :as services} :- APIHandlerServices]
   (let [external-ids (mapcat :external_ids import-data)]
     (log/debugf "Searching %s matching these external_ids %s"
                 entity-type
                 (pr-str external-ids))
     (if (seq external-ids)
       (debug (format "Results for %s:" (pr-str external-ids))
-             (all-pages entity-type external-ids auth-identity))
+             (all-pages entity-type external-ids auth-identity read-store))
       [])))
 
 (defn by-external-id

test/ctia/bundle/routes_test.clj
@@ -10,7 +10,7 @@
              [test :as t :refer [deftest is join-fixtures testing use-fixtures]]]
             [ctia.bulk.core :as bulk]
             [ctia.bundle.core :as core]
-            [ctia.store :refer [stores]]
+            [ctia.store :as store :refer [stores]]
             [ctia.properties :as p]
             [ctia.auth.capabilities :refer [all-capabilities]]
             [ctia.test-helpers
@@ -445,7 +445,8 @@
                                          "foouser"
                                          "foogroup"
                                          "user")
-     (let [duplicated-indicators (->> (mk-indicator 0)
+     (let [read-store store/read-store
+           duplicated-indicators (->> (mk-indicator 0)
                                       (repeat (* 10 core/find-by-external-ids-limit))
                                       (map #(assoc % :id (id/make-transient-id nil))))
            more-indicators (map mk-indicator (range 1 10))
@@ -459,7 +460,7 @@
                                  :body bundle
                                  :headers {"Authorization" "45c1f5e3f05d0"})
            ident (FakeIdentity. "foouser" ["foogroup"])
-           matched-entities (core/all-pages :indicator all-external-ids ident)
+           matched-entities (core/all-pages :indicator all-external-ids ident read-store)
            max-matched (+ core/find-by-external-ids-limit
                           (count more-indicators))]
        (assert (= 200 (:status response-create)))