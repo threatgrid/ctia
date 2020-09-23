d83621943d9c8e8fec7b1bb85ade77d719f16582
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-09-23 09:20:20 -0400

    feedback

--- src/ctia/init.clj
+++ src/ctia/init.clj
@@ -81,15 +81,14 @@
 (defn default-services
   "Returns the default collection of CTIA services based on provided properties."
   [config]
-  (let [auth-svc
-        (let [{auth-service-type :type :as auth} (get-in config [:ctia :auth])]
-          (case auth-service-type
-            :allow-all allow-all/allow-all-auth-service
-            :threatgrid threatgrid/threatgrid-auth-service
-            :static static-auth/static-auth-service
-            (throw (ex-info "Auth service not configured"
-                            {:message "Unknown service"
-                             :requested-service auth-service-type}))))]
+  (let [{auth-service-type :type} (get-in config [:ctia :auth])
+        auth-svc (case auth-service-type
+                   :allow-all allow-all/allow-all-auth-service
+                   :threatgrid threatgrid/threatgrid-auth-service
+                   :static static-auth/static-auth-service
+                   (throw (ex-info "Auth service not configured"
+                                   {:message "Unknown service"
+                                    :requested-service auth-service-type})))]
     [auth-svc]))
 
 
@@ -97,10 +96,9 @@
   "Lower-level Trapperkeeper booting function for
   custom services and config."
   [{:keys [services config]}]
-  (let [_ (validate-entities)
-        _ (log-properties config)
-        app (tk/boot-services-with-config services config)]
-    app))
+  (validate-entities)
+  (log-properties config)
+  (tk/boot-services-with-config services config))
 
 (defn start-ctia!
   "Does the heavy lifting for ctia.main (ie entry point that isn't a class).
