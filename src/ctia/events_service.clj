5e82c78522daef21edb9bc20ebcdf4cd69f7cf53
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-09-24 15:40:32 -0400

    cleaner

---
+++ src/ctia/events_service.clj
@@ -0,0 +1,30 @@
+(ns ctia.events-service
+  (:require [ctia.events-service-core :as core]
+            [puppetlabs.trapperkeeper.core :as tk]
+            [puppetlabs.trapperkeeper.services :refer [service-context]]))
+
+(defprotocol EventsService
+  (central-channel [this] "Returns the central channel")
+  (send-event [this event] [this chan event]
+              "Send an event to a channel. Use the central channel by default")
+  (register-listener [this listen-fn mode]
+                     [this pred listen-fn mode]
+                     "Convenience wrapper for registering a listener on the central event channel."))
+
+(tk/defservice events-service
+  EventsService
+  []
+  (init [this context] (core/init context))
+  (stop [this context] (core/stop context))
+  
+  (central-channel [this] (core/get-central-channel (service-context this)))
+  (send-event [this event] (core/send-event (service-context this) event))
+  (send-event [this chan event] (core/send-event (service-context this) chan event))
+  (register-listener
+    [this listen-fn mode] (core/register-listener
+                            (service-context this)
+                            listen-fn mode))
+  (register-listener
+    [this pred listen-fn mode] (core/register-listener
+                                 (service-context this)
+                                 pred listen-fn mode)))
