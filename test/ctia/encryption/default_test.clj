2762536d99da4f1a074e99cb8f27889c4bd63ab7
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-09-24 11:27:16 -0400

    use thrown-with-msg?

--- test/ctia/encryption/default_test.clj
+++ test/ctia/encryption/default_test.clj
@@ -8,14 +8,13 @@
 (deftest encryption-default-record-test
   (let [key-path "resources/cert/ctia-encryption.key"]
     (testing "failed service init"
-      (assert
-        (try (with-app-with-config app
-               [sut/default-encryption-service]
-               {:ctia {:encryption nil}}
-               false)
-             (catch Throwable e
-               (is (= (.getMessage e)
-                      "Assert failed: no secret or key filepath provided\n(or secret (:filepath key))"))))))
+      (is
+        (thrown-with-msg?
+          AssertionError
+          #"no secret or key filepath provided"
+          (with-app-with-config app
+            [sut/default-encryption-service]
+            {:ctia {:encryption nil}}))))
     (with-app-with-config app
       [sut/default-encryption-service]
       {:ctia {:encryption {:key {:filepath key-path}}}}
