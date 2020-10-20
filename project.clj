a7e850faba487b79fe98a1ba15ed641084bae3c5
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-10-20 10:52:25 -0400

    doc

--- project.clj
+++ project.clj
@@ -204,9 +204,9 @@
                             (clojure.string/join
                               "\n"
                               ["Welcome to CTIA!"
-                               "(go)    => start or restart CTIA"
-                               "(start) => start CTIA, if not already started"
-                               "(stop)  => stop CTIA, if not already stopped"]))}
+                               " (go)    => start or restart CTIA"
+                               " (start) => start CTIA, if not already started"
+                               " (stop)  => stop CTIA, if not already stopped"]))}
   :middleware [lein-git-down.plugin/inject-properties]
   ;; lein-git-down config
   :repositories [["public-github" {:url "git://github.com"}]
