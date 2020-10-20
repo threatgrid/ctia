50d1d6834eb7340ef14db472efcbc79bea73c951
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-10-20 16:53:08 -0400

    rename test helpers *-services => *-service-map

--- test/ctia/auth/threatgrid_test.clj
+++ test/ctia/auth/threatgrid_test.clj
@@ -1,21 +1,21 @@
 (ns ctia.auth.threatgrid-test
   (:require [ctia.auth.threatgrid :as sut]
-            [ctia.store-service-test :refer [store-services]]
+            [ctia.store-service-test :refer [store-service-map]]
             [clojure.test :refer [deftest is testing]]
             [puppetlabs.trapperkeeper.app :as app]
             [puppetlabs.trapperkeeper.testutils.bootstrap :refer [with-app-with-config]])
   (:import [java.util UUID]))
 
-(defn threatgrid-auth-whoami-url-services
+(defn threatgrid-auth-whoami-url-service-map
   "Service map for #'sut/threatgrid-auth-whoami-url-service"
   []
   {:ThreatgridAuthWhoAmIURLService sut/threatgrid-auth-whoami-url-service})
 
-(defn threatgrid-auth-services
+(defn threatgrid-auth-service-map
   "Service map for #'sut/threatgrid-auth-service"
   []
-  (merge (threatgrid-auth-whoami-url-services)
-         (store-services)
+  (merge (threatgrid-auth-whoami-url-service-map)
+         (store-service-map)
          {:IAuth sut/threatgrid-auth-service}))
 
 (deftest threatgrid-auth-whoami-url-service-test
@@ -23,7 +23,7 @@
                       (repeatedly 10
                                   #(str "https://cisco.com/" (UUID/randomUUID))))
                 (assert "Must define cases"))
-          :let [services (vals (threatgrid-auth-whoami-url-services))]]
+          :let [services (vals (threatgrid-auth-whoami-url-service-map))]]
     (testing ":whoami-url configuration corresponds to (get-whoami-url)"
       (with-app-with-config app services
         {:ctia {:auth {:threatgrid {:whoami-url url}}}}
--- test/ctia/store_service_test.clj
+++ test/ctia/store_service_test.clj
@@ -1,7 +1,7 @@
 (ns ctia.store-service-test
   (:require [ctia.store-service :as sut]))
 
-(defn store-services
+(defn store-service-map
   "Service map for #'sut/store-service"
   []
   {:StoreService sut/store-service})
