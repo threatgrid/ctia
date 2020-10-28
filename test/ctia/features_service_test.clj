4b48e5129252350e904ee4b6588d999fd797321d
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-10-27 23:32:46 -0500

    convolute sexps

--- test/ctia/features_service_test.clj
+++ test/ctia/features_service_test.clj
@@ -3,31 +3,31 @@
              [ctia.test-helpers.core :as th]))
 
 (deftest features-service-test
-  (th/with-properties ["ctia.features.disable" "asset,actor,sighting"]
-   (testing "FeaturesService methods for entity marked as disabled in config"
-     (th/fixture-ctia-with-app
-      (fn [app]
-        (let [{:keys [disabled? enabled?]} (th/get-service-map app :FeaturesService)]
-          (testing "Asset, Actor and Sighting are disabled"
-            (is (not-any? enabled? [:asset :actor :sighting])))
-          (testing "Incident, Indicator entities are enabled"
-            (is (every? enabled? [:incident :indicator])))))))))
+  (testing "FeaturesService methods for entity marked as disabled in config"
+    (th/with-properties ["ctia.features.disable" "asset,actor,sighting"]
+      (th/fixture-ctia-with-app
+       (fn [app]
+         (let [{:keys [disabled? enabled?]} (th/get-service-map app :FeaturesService)]
+           (testing "Asset, Actor and Sighting are disabled"
+             (is (not-any? enabled? [:asset :actor :sighting])))
+           (testing "Incident, Indicator entities are enabled"
+             (is (every? enabled? [:incident :indicator])))))))))
 
 (deftest routes-for-disabled-entities-test
-  (th/with-properties ["ctia.features.disable" "asset,actor,sighting"]
-   (testing "No http routes should exist for disabled entities"
-     (th/fixture-ctia-with-app
-      (fn [app]
-        (are [entity status-code] (->> entity
-                                       name
-                                       (format "ctia/%s/search")
-                                       (th/GET app)
-                                       :status
-                                       (= status-code))
+  (testing "No http routes should exist for disabled entities"
+    (th/with-properties ["ctia.features.disable" "asset,actor,sighting"]
+      (th/fixture-ctia-with-app
+       (fn [app]
+         (are [entity status-code] (->> entity
+                                        name
+                                        (format "ctia/%s/search")
+                                        (th/GET app)
+                                        :status
+                                        (= status-code))
 
-          :asset            404
-          :actor            404
-          :sighting         404
-          :asset-mapping    200
-          :indicator        200
-          :asset-properties 200))))))
+           :asset            404
+           :actor            404
+           :sighting         404
+           :asset-mapping    200
+           :indicator        200
+           :asset-properties 200))))))
