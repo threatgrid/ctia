c09cb05642f0629f1abe23a814514d820b89eb86
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-11-02 17:28:13 -0500

    print comparison

--- scripts/summarize-tests.clj
+++ scripts/summarize-tests.clj
@@ -6,16 +6,28 @@
   (:import [java.io File]))
 
 (defn summarize []
-  (println
-    "Test summary:\n"
-    (with-out-str
-      (pp/pprint
-        (->> (file-seq (io/file "target/test-results"))
-             (filter (fn [^File f]
-                       (and (.isFile f)
-                            (.startsWith (.getName f)
-                                         "ns-timing"))))
-             (map (comp read-string slurp))
-             (apply merge))))))
+  (let [timing (->> (file-seq (io/file "target/test-results"))
+                    (filter (fn [^File f]
+                              (and (.isFile f)
+                                   (.startsWith (.getName f)
+                                                "ns-timing"))))
+                    (map (comp read-string slurp))
+                    (apply merge))]
+    (when-some [expected (some-> (io/file "dev-resources/ctia_test_timings.edn")
+                                 slurp
+                                 read-string)]
+      (println (str "Expected test duration: "
+                    (/ (apply + (map :elapsed-ns (vals expected)))
+                       1e9)
+                    " seconds"))
+      (println (str "Actual test duration: "
+                    (/ (apply + (map :elapsed-ns (vals timing)))
+                       1e9)
+                    " seconds")))
+    (println
+      "Test summary:\n"
+      (with-out-str
+        (pp/pprint
+          timing)))))
 
 (summarize)
