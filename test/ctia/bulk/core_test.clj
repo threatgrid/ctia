247d61a22640e827c064a69c478e065b57c401ac
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-11-13 09:19:07 -0500

    fix bulk test regression

--- test/ctia/bulk/core_test.clj
+++ test/ctia/bulk/core_test.clj
@@ -1,5 +1,6 @@
 (ns ctia.bulk.core-test
-  (:require [ctia.bulk.core :as sut :refer [read-fn]]
+  (:require [ctia.auth.allow-all :refer [identity-singleton]]
+            [ctia.bulk.core :as sut :refer [read-fn]]
             [ctia.test-helpers.core :as helpers]
             [clj-momo.test-helpers.core :as mth]
             [clojure.test :refer [deftest testing is use-fixtures]]))
@@ -10,7 +11,8 @@
   (testing "Attempting to read an unreachable entity should not throw"
     (let [get-in-config (helpers/build-get-in-config-fn)
           res (sut/read-entities ["judgement-123"]
-                                 :judgement {}
+                                 :judgement
+                                 identity-singleton
                                  {:ConfigService {:get-in-config get-in-config}
                                   :StoreService {:read-store (constantly nil)}})]
       (is (= [nil] res)))))
