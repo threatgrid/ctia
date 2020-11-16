ca005eef36b8b55e660b57e8c9c3066edd31fee6
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-11-16 12:24:06 -0500

    fix

--- src/ctia/status/routes.clj
+++ src/ctia/status/routes.clj
@@ -6,10 +6,9 @@
    [ring.util.http-response :refer [ok]]))
 
 (defn status-routes []
-  (routes
-    (context "/status" []
-             :tags ["Status"]
-             (GET "/" []
-                  :return StatusInfo
-                  :summary "Health Check"
-                  (ok {:status :ok})))))
+  (context "/status" []
+           :tags ["Status"]
+           (GET "/" []
+                :return StatusInfo
+                :summary "Health Check"
+                (ok {:status :ok}))))
