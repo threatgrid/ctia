54d17f48617bc3600437ff296156c6b8a1e6d7d0
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-11-30 14:12:31 -0500

    ductile dep

--- project.clj
+++ project.clj
@@ -59,7 +59,7 @@
 
                  [threatgrid/ctim "1.0.22"]
                  [threatgrid/clj-momo "0.3.5"]
-                 [threatgrid/ductile "0.2.0"]
+                 [threatgrid/ductile "ef4d93a11b81e635eca7da3aec909cb1dcb7d6b1"]
 
                  [com.arohner/uri "0.1.2"]
 
