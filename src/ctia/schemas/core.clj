24238b01fc8e50a0506d45fdc2af73272590dcde
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-12-08 14:34:24 -0600

    re-asssign entity owner on the event update (#1047)
    Re-assign owner on the event update flow
    
    > **Epic** #
    > Close https://github.com/threatgrid/iroh/issues/3596
    > Related #
    
    <!--
    
    Describe your PR for reviewers.
    Don't forget to set correct labels (User Facing / Beta / Feature Flag)
    If there is UI change please add a screen capture.
    -->
    
    <!-- UNCOMMENT THIS SECTION IF NEEDED
    <a name="iroh-services-clients">[§](#iroh-services-clients)</a> IROH Services Clients
    =====================================================================================
    
    Put all informations that need to be communicated to IROH Services Clients.
    Typically IROH-UI, ATS Integration, Orbital, etc...
     -->
    
    <a name="qa">[§](#qa)</a> QA
    ============================
    
    <!--
    Describe the steps to test your PR.
    
    1.
    2.
    3.
    
    Or if no QA is needed keep it as is.
     -->
    No QA is needed.
    
    <!-- UNCOMMENT THIS SECTION IF NEEDED
    <a name="ops">[§](#ops)</a> Ops
    ===============================
    
      Specify Ops related issues and documentation
    - Config change needed: threatgrid/tenzin#
    - Migration needed: threatgrid/tenzin#
    - How to enable/disable that feature: (ex remove service from `bootstrap.cfg`, add scope to org)
    -->
    
    <!-- UNCOMMENT THIS SECTION IF NEEDED
    <a name="documentation">[§](#documentation)</a> Documentation
    =============================================================
    
      Public Facing documentation section;
    - Public documentation updated needed: threatgrid/iroh-ui#
      See internal [doc file](./services/iroh-auth/doc/public-doc.org)
     -->
    
    <a name="release-notes">[§](#release-notes)</a> Release Notes
    =============================================================
    
    <!-- REMOVE UNUSED LINES -->
    
    ```
    intern: one-line description for internal eyes only.
    public: one line description for public release notes.
    ```
    
    <a name="squashed-commits">[§](#squashed-commits)</a> Squashed Commits
    ======================================================================
    
    abe0bb93 * origin/incorrect_event_owner_#3596 put the require back, removing it breaks the code
    3291a681 * rename alias so it's less ambiguous
    d0c32ef9 * Update test/ctia/flows/crud_test.clj
    a46af6a7 * Update test/ctia/entity/event_test.clj
    c6f15788 * refactor and cleanup the test
    75da7cbb * update a test
    ffb1ede1 * 1047 re-asssign entity owner on the event update

--- src/ctia/schemas/core.clj
+++ src/ctia/schemas/core.clj
@@ -19,25 +19,25 @@
 
 (s/defschema APIHandlerServices
   "Maps of services available to routes"
-  {:ConfigService {:get-config (s/=> s/Any s/Any)
-                   :get-in-config (s/=>* s/Any
-                                         [[s/Any]]
-                                         [[s/Any] s/Any])}
-   :CTIAHTTPServerService {:get-port (s/=> Port)
-                           :get-graphql (s/=> graphql.GraphQL)}
-   :HooksService {:apply-hooks (s/pred ifn?) ;;keyword varargs
-                  :apply-event-hooks (s/=> s/Any s/Any)}
-   :StoreService {:read-store (s/pred ifn?) ;;varags
-                  :write-store (s/pred ifn?)} ;;varags
-   :IAuth {:identity-for-token (s/=> s/Any s/Any)}
-   :GraphQLNamedTypeRegistryService {:get-or-update-named-type-registry
-                                     (s/=> graphql.schema.GraphQLType
-                                           s/Str
-                                           (s/=> graphql.schema.GraphQLType))}
-   :IEncryption {:encrypt (s/=> s/Any s/Any)
-                 :decrypt (s/=> s/Any s/Any)}
-   :FeaturesService {:enabled? (s/=> s/Keyword s/Bool)
-                     :feature-flags (s/=> s/Any [s/Str])}})
+  {:ConfigService                             {:get-config    (s/=> s/Any s/Any)
+                                               :get-in-config (s/=>* s/Any
+                                                                     [[s/Any]]
+                                                                     [[s/Any] s/Any])}
+   :CTIAHTTPServerService                     {:get-port    (s/=> Port)
+                                               :get-graphql (s/=> graphql.GraphQL)}
+   :HooksService                              {:apply-hooks       (s/pred ifn?) ;;keyword varargs
+                                               :apply-event-hooks (s/=> s/Any s/Any)}
+   :StoreService                              {:read-store  (s/pred ifn?) ;;varags
+                                               :write-store (s/pred ifn?)} ;;varags
+   :IAuth                                     {:identity-for-token (s/=> s/Any s/Any)}
+   :GraphQLNamedTypeRegistryService           {:get-or-update-named-type-registry
+                                               (s/=> graphql.schema.GraphQLType
+                                                     s/Str
+                                                     (s/=> graphql.schema.GraphQLType))}
+   :IEncryption                               {:encrypt (s/=> s/Any s/Any)
+                                               :decrypt (s/=> s/Any s/Any)}
+   :FeaturesService                           {:enabled?      (s/=> s/Keyword s/Bool)
+                                               :feature-flags (s/=> s/Any [s/Str])}})
 
 (s/defschema HTTPShowServices
   {:ConfigService {:get-in-config (s/=>* s/Any
