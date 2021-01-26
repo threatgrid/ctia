e37c635d8ea3f4ac96e715b3d7eddedf0fc2eba3
Ag Ibragimov <agzam.ibragimov@gmail.com>
2021-01-25 20:08:52 -0600

    Adds graphql schemas for asset-mapping

--- src/ctia/graphql/schemas.clj
+++ src/ctia/graphql/schemas.clj
@@ -1,7 +1,7 @@
 (ns ctia.graphql.schemas
   (:require
    [ctia.entity.asset-mapping.graphql-schemas :as asset-mapping :refer [AssetMappingType AssetMappingConnectionType]]
-   [ctia.entity.asset-properties.graphql-schemas :as asset-properties :refer [AssetPropertiesType AssetPropertiesConnectionType]]
+   [ctia.entity.asset-properties.graphql-schemas :as asset-properties :refer [AssetPropertiesConnectionType]]
    [ctia.entity.asset.graphql-schemas :as asset :refer [AssetType AssetConnectionType]]
    [ctia.entity.attack-pattern :as attack-pattern :refer [AttackPatternConnectionType AttackPatternType]]
    [ctia.entity.casebook :as casebook :refer [CasebookConnectionType CasebookType]]
