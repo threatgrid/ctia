de05be62284a82ffdb3b4a75fe72bcb4bbc10707
Guillaume ERETEO <gereteo@cisco.com>
2021-02-10 11:25:27 +0100

    ctim 1.1.2
    wip
    
    1.1.2

--- project.clj
+++ project.clj
@@ -60,7 +60,7 @@
                  [metosin/schema-tools "0.12.2"]
                  [threatgrid/flanders "0.1.23"]
 
-                 [threatgrid/ctim "1.1.1"]
+                 [threatgrid/ctim "1.1.2"]
                  [threatgrid/clj-momo "0.3.5"]
                  [threatgrid/ductile "0.3.0"]
 
