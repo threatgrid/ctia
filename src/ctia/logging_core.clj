7c1b21db4599544c13272b3a0e6cd045a7538357
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-03-08 10:41:01 -0500

    Fix flaky logger test (#1082)
    <!-- Specify linked issues and REMOVE THE UNUSED LINES -->
    
    Close https://github.com/threatgrid/iroh/issues/4903
    
    <!--
    
    Describe your PR for reviewers.
    Don't forget to set correct labels (User Facing / Beta / Feature Flag)
    If there is UI change please add a screen capture.
    -->
    
    <!-- UNCOMMENT THIS SECTION IF NEEDED
    <a name="iroh-services-clients">[§](#iroh-services-clients)</a> IROH Services Clients
    =====================================================================================
    
    Put all informations that need to be communicated to IROH Services Clients.
    Typically IROH-UI, ATS Integration, Orbital, etc...
     -->
    
    <a name="qa">[§](#qa)</a> QA
    ============================
    
    <!--
    Describe the steps to test your PR.
    
    1.
    2.
    3.
    
    Or if no QA is needed keep it as is.
     -->
    No QA is needed.
    
    <!-- UNCOMMENT THIS SECTION IF NEEDED
    <a name="ops">[§](#ops)</a> Ops
    ===============================
    
      Specify Ops related issues and documentation
    - Config change needed: threatgrid/tenzin#
    - Migration needed: threatgrid/tenzin#
    - How to enable/disable that feature: (ex remove service from `bootstrap.cfg`, add scope to org)
    -->
    
    <!-- UNCOMMENT THIS SECTION IF NEEDED
    <a name="documentation">[§](#documentation)</a> Documentation
    =============================================================
    
      Public Facing documentation section;
    - Public documentation updated needed: threatgrid/iroh-ui#
      See internal [doc file](./services/iroh-auth/doc/public-doc.org)
     -->
    
    <a name="release-notes">[§](#release-notes)</a> Release Notes
    =============================================================
    
    <!-- REMOVE UNUSED LINES -->
    
    ```
    intern: Fix flaky logger test
    ```
    
    <a name="squashed-commits">[§](#squashed-commits)</a> Squashed Commits
    ======================================================================

--- src/ctia/logging_core.clj
+++ src/ctia/logging_core.clj
@@ -2,8 +2,10 @@
   (:require [clojure.core.async :as a]
             [clojure.tools.logging :as log]))
 
+(def logging-prefix "event:")
+
 (defn start [context register-listener-fn]
-  (assoc context :control (register-listener-fn #(log/info "event:" %) :blocking)))
+  (assoc context :control (register-listener-fn #(log/info logging-prefix %) :blocking)))
 
 (defn stop [{:keys [control] :as context}]
   (some-> control a/close!)
--- test/ctia/logger_test.clj
+++ test/ctia/logger_test.clj
@@ -3,6 +3,7 @@
              [core :as test-helpers]
              [es :as es-helpers]]
             [ctia.entity.event.obj-to-event :as o2e]
+            [ctia.logging-core :refer [logging-prefix]]
             [clojure.test :refer [deftest is use-fixtures]]
             [schema.test :as st]
             [clojure.tools.logging :as log]
@@ -17,13 +18,14 @@
 (deftest test-logged
   (let [app (test-helpers/get-current-app)
         {:keys [send-event]} (test-helpers/get-service-map app :EventsService)
-        sb (StringBuilder.)
+        events-atom (atom [])
         patched-log (fn [_logger
                          _level
                          _throwable
-                         message]
-                      (.append sb message)
-                      (.append sb "\n"))]
+                         ^String message]
+                      (when (.startsWith message logging-prefix)
+                        ;; grab the edn after the logging-prefix
+                        (swap! events-atom conj (read-string (subs message (count logging-prefix))))))]
     (with-redefs [log/log* patched-log]
       (send-event (o2e/to-create-event
                      {:owner "tester"
@@ -44,11 +46,28 @@
                      "test-2"
                      "tester"))
       (Thread/sleep 100)   ;; wait until the go loop is done
-      (let [scrubbed (-> (str sb)
-                         (str/replace #"#inst \"[^\"]*\"" "#inst \"\"")
-                         (str/replace #":id event[^,]*" ":id event")
-                         (str/replace #"Lifecycle worker completed :boot lifecycle task; awaiting next task.\s"
-                                      ""))
-            expected
-            "event: {:owner tester, :groups [foo], :entity {:owner tester, :groups [foo], :id test-1, :type :test, :tlp green, :data 1}, :timestamp #inst \"\", :id test-1, :type event, :tlp green, :event_type :record-created}\nevent: {:owner tester, :groups [foo], :entity {:owner tester, :groups [foo], :id test-2, :type :test, :tlp green, :data 2}, :timestamp #inst \"\", :id test-2, :type event, :tlp green, :event_type :record-created}\n"]
-        (is (= expected scrubbed))))))
+      (is (= '[{:owner tester,
+                :groups [foo],
+                :entity {:owner tester,
+                         :groups [foo],
+                         :id test-1,
+                         :type :test,
+                         :tlp green,
+                         :data 1},
+                :id test-1,
+                :type event,
+                :tlp green,
+                :event_type :record-created}
+               {:owner tester,
+                :groups [foo], 
+                :entity {:owner tester, 
+                         :groups [foo], 
+                         :id test-2, 
+                         :type :test, 
+                         :tlp green, 
+                         :data 2}, 
+                :id test-2,
+                :type event, 
+                :tlp green,
+                :event_type :record-created}]
+             (map #(dissoc % :timestamp) @events-atom))))))
