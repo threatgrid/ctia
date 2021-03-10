030306568416ab5cce5e9a7d355d20d91e15ac99
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-03-10 16:55:21 -0500

    fix

--- test/ctia/stores/es/wait_for_test.clj
+++ test/ctia/stores/es/wait_for_test.clj
@@ -1,43 +1,31 @@
 (ns ctia.stores.es.wait-for-test
-  (:require [ctia.auth.capabilities :refer [all-capabilities]]
+  (:require [clojure.test :refer [deftest is use-fixtures]]
+            [clj-momo.test-helpers.core :as mth]
+            [ctia.auth.capabilities :refer [all-capabilities]]
+            [ctia.entity.incident :refer [incident-entity]]
             [ctia.test-helpers.core :as helpers]
             [ctia.test-helpers.crud :refer [crud-wait-for-test]]
             [ctia.test-helpers.fake-whoami-service :as whoami-helpers]
             [ctia.test-helpers.store :refer [test-selected-stores-with-app]]
+            [ctim.examples.incidents :refer [new-incident-maximal]]
+            [puppetlabs.trapperkeeper.app :as app]))
 
-            [ctia.entity.incident :refer [incident-entity]]
-            [ctim.examples.incidents
-             :refer
-             [new-incident-maximal new-incident-minimal]]))
-
-(def new-judgement
-  (merge ex/new-judgement-maximal
-         {:observable {:value "1.2.3.4"
-                       :type "ip"}
-          :external_ids ["http://ex.tld/ctia/judgement/judgement-123"
-                         "http://ex.tld/ctia/judgement/judgement-456"]
-          :disposition 2
-          :disposition_name "Malicious"
-          :source "test"
-          :priority 100
-          :severity "High"
-          :confidence "Low"
-          :reason "This is a bad IP address that talked to some evil servers"}))
+(use-fixtures :once
+              mth/fixture-schema-validation
+              whoami-helpers/fixture-server)
 
 ;; we choose incidents to test wait_for because it supports patches and
 ;; thus achieves full coverage of crud-wait-for-test
 (deftest test-wait_for
-  (let [ran? (atom false)]
-    (test-selected-stores-with-app
-      (fn [app]
-        (helpers/set-capabilities! app "foouser" ["foogroup"] "user" all-capabilities)
-        (whoami-helpers/set-whoami-response app "45c1f5e3f05d0" "foouser" "foogroup" "user")
-        (let [{:keys [entity] :as parameters} (into incident-entity
-                                                    {:app app
-                                                     :example new-incident-maximal
-                                                     :headers {:Authorization "45c1f5e3f05d0"}})]
-          (when (= "es"
-                   (get-in-config [:ctia :store entity]))
-            (crud-wait-for-test parameters)
-            (reset! ran? true)))))
-    (is @ran?)))
+  (test-selected-stores-with-app
+    #{:es-store}
+    (fn [app]
+      (helpers/set-capabilities! app "foouser" ["foogroup"] "user" (all-capabilities))
+      (whoami-helpers/set-whoami-response app "45c1f5e3f05d0" "foouser" "foogroup" "user")
+      (let [{{:keys [get-in-config]} :ConfigService} (app/service-graph app)
+            {:keys [entity] :as parameters} (into incident-entity
+                                                  {:app app
+                                                   :example new-incident-maximal
+                                                   :headers {:Authorization "45c1f5e3f05d0"}})]
+        (is (= "es" (get-in-config [:ctia :store entity])))
+        (crud-wait-for-test parameters)))))
