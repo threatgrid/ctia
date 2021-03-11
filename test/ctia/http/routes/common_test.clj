ddeceeb2d5744cd7df508d39de93f46f286bf68d
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-03-11 11:25:25 -0500

    feedback

--- test/ctia/http/routes/common_test.clj
+++ test/ctia/http/routes/common_test.clj
@@ -1,8 +1,20 @@
 (ns ctia.http.routes.common-test
-  (:require [ctia.http.routes.common :as sut]
-            [clj-momo.lib.clj-time.coerce :as tc]
+  (:require [clj-momo.lib.clj-time.coerce :as tc]
             [clj-momo.lib.clj-time.core :as t]
-            [clojure.test :refer [are is deftest testing]]))
+            [clj-momo.test-helpers.core :as mth]
+            [clojure.test :refer [are is deftest testing use-fixtures]]
+            [ctia.auth.capabilities :refer [all-capabilities]]
+            [ctia.entity.incident :refer [incident-entity]]
+            [ctia.http.routes.common :as sut]
+            [ctia.test-helpers.core :as helpers]
+            [ctia.test-helpers.crud :refer [crud-wait-for-test]]
+            [ctia.test-helpers.store :refer [test-selected-stores-with-app]]
+            [ctim.examples.incidents :refer [new-incident-maximal]]
+            [puppetlabs.trapperkeeper.app :as app]))
+
+(use-fixtures :once
+              mth/fixture-schema-validation
+              whoami-helpers/fixture-server)
 
 (deftest coerce-date-range
   (with-redefs [sut/now (constantly (tc/from-string "2020-12-31"))]
@@ -178,3 +190,20 @@
                (sut/capabilities->string v))
          nil
          #{})))
+
+;; we choose incidents to test wait_for because it supports patches and
+;; thus achieves full coverage of crud-wait-for-test
+(deftest test-wait_for
+  (test-selected-stores-with-app
+    #{:es-store}
+    (fn [app]
+      (helpers/set-capabilities! app "foouser" ["foogroup"] "user" (all-capabilities))
+      (whoami-helpers/set-whoami-response app "45c1f5e3f05d0" "foouser" "foogroup" "user")
+      (let [{{:keys [get-in-config]} :ConfigService} (app/service-graph app)
+            {:keys [entity] :as parameters} (into incident-entity
+                                                  {:app app
+                                                   :example new-incident-maximal
+                                                   :headers {:Authorization "45c1f5e3f05d0"}})
+            entity-store (get-in-config [:ctia :store entity])]
+        (assert (= "es" entity-store) (pr-str entity-store))
+        (crud-wait-for-test parameters)))))
