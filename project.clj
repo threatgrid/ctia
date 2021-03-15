3c6b1e71b456148eb150fa62a2b50ee9a44cd90c
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-03-15 14:06:09 -0400

    fix

--- project.clj
+++ project.clj
@@ -160,7 +160,6 @@
                    :resource-paths ["test/resources"]
                    :source-paths ["dev"]}
              :ci {:pedantic? :abort
-                  :jvm-opts ["-Dctia.dev.cron=true"]
                   :global-vars {*warn-on-reflection* true}}
              :cron {:jvm-opts ["-Dctia.dev.cron=true"]}
              :jmx {:jvm-opts ["-Dcom.sun.management.jmxremote"
