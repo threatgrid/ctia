7eaad299f4701c345156acaa06f6a07e80ce7d7d
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-03-17 18:53:18 -0400

    rm trampoline

--- scripts/actions/setup-env.clj
+++ scripts/actions/setup-env.clj
@@ -1,6 +1,8 @@
 #!/usr/bin/env bb
 
-(defn getenv ^String [^String s] (System/getenv ^String s))
+(require '[clojure.java.io :as io])
+
+(defn getenv ^String [^String s] (System/getenv s))
 
 (defn add-env
   "Add env var k=v to future GitHub Actions steps in this job"
@@ -10,13 +12,13 @@
         (str k "=" v "\n")
         :append true))
 
-(-> (getenv "LOG_PATH") File. .mkdirs)
+(io/make-parents (getenv "LOG_PATH"))
 
-(assert (not (System/getenv "TRAVIS_EVENT_TYPE"))
+(assert (not (getenv "TRAVIS_EVENT_TYPE"))
         "Actions only")
 
 (add-env "CTIA_TEST_SUITE"
-         (if (= (System/getenv "GITHUB_EVENT_NAME")
+         (if (= (getenv "GITHUB_EVENT_NAME")
                 "schedule")
            "cron"
            "ci"))
