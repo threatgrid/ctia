d7ddb0eab831c6d9c7c59ef56bebcf85c05946c0
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-05-20 19:11:29 -0400

    setup_env.clj

---
+++ scripts/actions/setup_env.clj
@@ -0,0 +1,21 @@
+#!/usr/bin/env bb
+
+;; Example:
+;; LOG_PATH=$(mktemp -d) ./scripts/actions/setup-env.clj
+
+(ns actions.setup-env
+  (:require [actions.actions-helpers :refer [add-env getenv]]
+            [clojure.java.shell :as sh]))
+
+(defn -main [& _args]
+  (let [log-path (getenv "LOG_PATH")]
+    (assert log-path)
+    (-> (sh/sh "mkdir" "-p" log-path)
+        :exit
+        #{0}
+        assert))
+
+  (assert (not (getenv "TRAVIS_EVENT_TYPE")) "Actions only"))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (-main))
