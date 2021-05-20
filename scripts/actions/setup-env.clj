e857333c8770e61cde2a49db41176bca952ebeff
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2021-05-20 17:19:52 -0400

    {:test-suite :cron}

--- scripts/actions/setup-env.clj
+++ scripts/actions/setup-env.clj
@@ -1,8 +1,17 @@
 #!/usr/bin/env bb
 
-(ns actions.setup-env
-  (:require [actions.actions-helpers :refer [add-env getenv]]))
+;; Example:
+;; LOG_PATH=$(mktemp -d) ./scripts/actions/setup-env.clj
 
-(-> (getenv "LOG_PATH") File. .mkdirs assert)
+(ns actions.setup-env
+  (:require [actions.actions-helpers :refer [add-env getenv]]
+            [clojure.java.shell :as sh]))
+
+(let [log-path (getenv "LOG_PATH")]
+  (assert log-path)
+  (-> (sh/sh "mkdir" "-p" log-path)
+      :exit
+      #{0}
+      assert))
 
 (assert (not (getenv "TRAVIS_EVENT_TYPE")) "Actions only")
