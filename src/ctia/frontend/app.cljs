e10b3147e6840b2a95c5cb47dceee9dc84dae3c1
Ag Ibragimov <agzam.ibragimov@gmail.com>
2021-07-19 15:14:49 -0500

    some primal data fetching and rendering

--- src/ctia/frontend/app.cljs
+++ src/ctia/frontend/app.cljs
@@ -1,14 +1,24 @@
 (ns ctia.frontend.app
-  (:require [reagent.dom :as re-dom]))
+  (:require
+   [reagent.dom :as re-dom]
+   [ctia.frontend.sample-chart :as sample-chart]
+   [re-frame.core :refer [dispatch
+                          reg-event-db]]))
 
-(defn ^:export init []
-  (println "init"))
+(reg-event-db
+ ::init
+ (fn [db _]
+   (assoc db :defaults {})))
 
 (defn root-view []
   [:div
-   [:h1 "I am Groot"]])
+   [sample-chart/root]])
 
 (defn ^:dev/after-load render! []
   (re-dom/render
    [root-view]
    (js/document.getElementById "app")))
+
+(defn init []
+  (dispatch [::init])
+  (render!))
