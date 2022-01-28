33b7b91aebf29711d31b966548f4557d95b7389a
Matthieu Sprunck (msprunck) <matthieu@sprunck.com>
2022-01-28 16:24:55 +0100

    Logging refinements

--- project.clj
+++ project.clj
@@ -151,7 +151,10 @@
                  ;; GraphQL
                  [base64-clj "0.1.1"]
                  [threatgrid/ring-graphql-ui "0.1.1"]
-                 [com.graphql-java/graphql-java "9.7"]]
+                 [com.graphql-java/graphql-java "9.7"]
+
+                 ;; Logging
+                 [org.slf4j/log4j-over-slf4j "1.7.20"]]
 
   :resource-paths ["resources" "doc"]
   :classpath ".:resources"
