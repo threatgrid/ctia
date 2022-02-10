db45542609b2172fd47a4849693c83c7e689322f
Guillaume ERETEO <gereteo@cisco.com>
2022-02-10 18:58:40 +0100

    wip

--- src/ctia/observable/routes.clj
+++ src/ctia/observable/routes.clj
@@ -1,25 +1,18 @@
 (ns ctia.observable.routes
-  (:require [ctia.observable.core :as core]
-            [ctia.lib.compojure.api.core :refer [GET routes]]
-            [ctia.domain.entities
+  (:require [ctia.domain.entities
              :refer
              [page-with-long-id short-id->long-id un-store-page]]
             [ctia.entity.judgement :refer [JudgementsByObservableQueryParams]]
             [ctia.entity.judgement.schemas :refer [PartialJudgementList]]
             [ctia.entity.sighting :refer [SightingsByObservableQueryParams]]
             [ctia.entity.sighting.schemas :refer [PartialSightingList]]
-            [ctia.http.routes.common :refer [paginated-ok PagingParams]
-             :as common]
-            [ctia.properties :as p]
+            [ctia.http.routes.common :as common :refer [paginated-ok PagingParams]]
+            [ctia.lib.compojure.api.core :refer [GET routes]]
+            [ctia.observable.core :as core]
             [ctia.schemas.core
              :refer
              [APIHandlerServices ObservableTypeIdentifier Reference Verdict]]
-            [ctia.store
-             :refer
-             [calculate-verdict
-              list-judgements-by-observable
-              list-records]]
-            [ctim.domain.id :as id]
+            [ctia.store :refer [calculate-verdict]]
             [ductile.pagination :as pag]
             [ring.swagger.schema :refer [describe]]
             [ring.util.http-response :refer [not-found ok]]
@@ -111,7 +104,7 @@
         :identity-map identity-map
         :return PartialSightingList
         :summary "Returns Sightings associated with the specified observable."
-        (-> (core/observables->sightings
+        (-> (core/observable->sightings
              {:type observable_type
               :value observable_value}
              identity-map
