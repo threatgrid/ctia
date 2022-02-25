0847d53350748887d530a2b1310b8c529b776993
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-02-25 13:37:42 -0500

    fix

--- project.clj
+++ project.clj
@@ -275,7 +275,7 @@
                           "run" "-m" "ctia.dev.split-tests/dir" :project/test-paths]
             "tests" ["with-profile" ~ci-profiles "run" "-m" "circleci.test"]
 
-            "ci-run-tests" ["with-profile" ~ci-profiles "do" "clean," "javac," "split-test" ":no-gen"]
+            "ci-run-tests" ["with-profile" ~ci-profiles "do" "clean," "javac," "split-test" ":frenchy64"]
             "cron-run-tests" ["with-profile" ~ci-profiles "do" "clean," "javac," "split-test" ":frenchy64"]
             "all-ci-profiles" ["shell" "echo" ~(pr-str all-ci-profiles)]
             ;; warm deps cache for all permutations of the build
