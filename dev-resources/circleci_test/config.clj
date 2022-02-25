0aea359efd61b1f81070f80c915590b2ae7e454c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-02-25 13:36:47 -0500

    selector

--- dev-resources/circleci_test/config.clj
+++ dev-resources/circleci_test/config.clj
@@ -3,7 +3,8 @@
 {:test-results-dir "target/test-results"
  :reporters [circleci.test.report/clojure-test-reporter
              ctia.dev.test-reporter/time-reporter]
- :selectors {:es-store :es-store
+ :selectors {:frenchy64 :frenchy64
+             :es-store :es-store
              :disabled :disabled
              :default (complement
                         (some-fn :disabled
