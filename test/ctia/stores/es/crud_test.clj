492b057872d991e0d9c021f0ab590c3796d26724
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-03-11 14:13:45 -0500

    :sort

--- test/ctia/stores/es/crud_test.clj
+++ test/ctia/stores/es/crud_test.clj
@@ -902,29 +902,30 @@
       (is (= expected (sut/with-default-sort-field es-params props))
           msg))))
 
-(deftest sort-params-ext-test
-  ;; example call
+(deftest parse-sort-params-op-test
   (is (=
-       {:sort
-        [{"Severity" {:order :asc}}
-         {:_script {:type "number"
-                    :script {:lang "painless"
-                             :inline (str "if (!doc.containsKey('Severity') || doc['Severity'].size() != 1) { return params.default }\n"
-                                          "return params.remappings.getOrDefault(doc['Severity'].value, params.default)")
-                             :params {;; note: lowercased
-                                      :remappings {"critical" 0, "high" 1}
-                                      :default 0}}
-                    :order :asc}}]}
-       (sut/sort-params-ext
-         [{:op :field
-           :field-name "Severity"
-           :sort_order :asc}
-          {:op :remap
-           :remap-type :number
-           :field-name "Severity"
-           ;; note: uppercased keys are lowercased
-           :remappings {"Critical" 0
-                        "High" 1}
-           :sort_order :asc
-           :remap-default 0}]
+       {"Severity" {:order :asc}}
+       (sut/parse-sort-params-op
+         {:op :field
+          :field-name "Severity"
+          :sort_order :asc}
+         :asc)))
+  (is (=
+       {:_script {:type "number"
+                  :script {:lang "painless"
+                           :inline (str "if (!doc.containsKey('Severity') || doc['Severity'].size() != 1) { return params.default }\n"
+                                        "return params.remappings.getOrDefault(doc['Severity'].value, params.default)")
+                           :params {;; note: lowercased
+                                    :remappings {"critical" 0, "high" 1}
+                                    :default 0}}
+                  :order :asc}}
+       (sut/parse-sort-params-op
+         {:op :remap
+          :remap-type :number
+          :field-name "Severity"
+          ;; note: uppercased keys are lowercased
+          :remappings {"Critical" 0
+                       "High" 1}
+          :sort_order :asc
+          :remap-default 0}
          :asc))))
