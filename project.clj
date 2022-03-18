7439ef863967e2d339d60494ebaf0c77b0adbeec
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-03-18 11:44:33 -0400

    override

--- project.clj
+++ project.clj
@@ -82,6 +82,7 @@
                  [metosin/schema-tools "0.12.2"]
                  [threatgrid/flanders "0.1.23"]
                  [threatgrid/ctim "1.1.11"]
+                 [instaparse "1.4.10"] ;; com.gfredericks/test.chuck > threatgrid/ctim
                  [threatgrid/clj-momo "0.3.5"]
                  [threatgrid/ductile "0.4.2"]
 
