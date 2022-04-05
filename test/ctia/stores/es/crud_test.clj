595fa7a4d4757fd818ec2143d0eeec348fa4ada0
Guillaume ERETEO <gereteo@cisco.com>
2022-04-05 11:14:37 +0200

    fix test working only on ES5

--- test/ctia/stores/es/crud_test.clj
+++ test/ctia/stores/es/crud_test.clj
@@ -84,17 +84,18 @@
     (testing "renaming fields"
       ;; when an entity has a mapping with keyword and it has a nested text field,
       ;; substitute keyword with the text field
-      (let [es-conn-state
-            (-> es-conn-state
-                (assoc-in [:services :FeaturesService :flag-value] (constantly "true"))
-                (assoc-in [:config :mappings "incident" :properties :kw-field]
-                          {:type "keyword" :fields {:text {:type "text"}}}))]
-        (are [queries exp]
-            (is (= exp (es.query/refine-full-text-query-parts
-                        es-conn-state queries)))
-          [{:query "*" :fields ["kw-field"]}]
-          [{:query_string {:query "*"
-                           :fields ["kw-field.text"]}}])))))
+      (let [fake-mapping {:kw-field
+                          {:type "keyword" :fields {:text {:type "text"}}}}
+            base-conn-state (assoc-in es-conn-state [:services :FeaturesService :flag-value] (constantly "true"))]
+        (doseq [es-version [5 7]]
+          (let [conn-state (cond-> (assoc-in base-conn-state [:props :version] es-version)
+                             (= es-version 5) (assoc-in [:config :mappings :incident :properties] fake-mapping)
+                             (= es-version 7) (assoc-in [:config :mappings :properties] fake-mapping))]
+            (are [queries exp]
+                (is (= exp (es.query/refine-full-text-query-parts conn-state queries)))
+              [{:query "*" :fields ["kw-field"]}]
+              [{:query_string {:query "*"
+                               :fields ["kw-field.text"]}}])))))))
 
 (deftest ensure-document-id-in-map-test
   (is (= {:id '("actor-677796fd-b5d2-46e3-b57d-4879bcca1ce7")}
