a01f9a851206f745ee6f792cac82344be67be477
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2022-04-07 16:17:09 -0400

    fix

--- test/ctia/stores/es/init_test.clj
+++ test/ctia/stores/es/init_test.clj
@@ -149,7 +149,7 @@
 
 (deftest get-existing-indices-test
   (let [indexname (gen-indexname)]
-    (h/with-config-transformer
+    (helpers/with-config-transformer
       #(assoc-in [:ctia :store :es :default :ctia.task.update-index-state/update-index-state-task] true)
       (for-each-es-version
         "get-existing-indices should retrieve existing indices if any."
