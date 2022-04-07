d311bc3ee5dab7d61f27c2ea39ccd20f31d06ab0
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2022-04-07 15:21:35 -0400

    checks

--- src/ctia/task/update_index_state.clj
+++ src/ctia/task/update_index_state.clj
@@ -1,10 +1,16 @@
 (ns ctia.task.update-index-state
   (:require [ctia.init :as init]
-            [ctia.properties :as p]))
+            [ctia.properties :as p]
+            [puppetlabs.trapperkeeper.internal :as internal]))
 
 (defn do-task []
-  (try (init/start-ctia! {:config (assoc-in (p/build-init-config) [:ctia :store :es :default ::update-index-state-task] true)})
-       0
+  (try (-> {:config (assoc-in (p/build-init-config)
+                              [:ctia :store :es :default ::update-index-state-task]
+                              true)}
+           init/start-ctia!
+           internal/shutdown
+           count ;; returns number of exceptions thrown
+           (min 1)) ;; exit 1 if exceptions, otherwise 0
        (catch Throwable _
          1)))
 
