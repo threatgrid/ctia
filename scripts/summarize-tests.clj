3ea1f63147a9fa85156fd35a869de6b126e9fd8c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-04-07 14:38:28 -0400

    Summarize test durations in minutes (#1241)
    <!-- Specify linked issues and REMOVE THE UNUSED LINES -->
    
    Top 10 slowest test with human readable timing. https://github.com/threatgrid/ctia/runs/5854822226?check_suite_focus=true#step:9:284
    
    ```clojure
    Test var summary (slowest to fastest):
    [[ctia.http.routes.graphql-test/test-graphql-route
      {:elapsed-ns 2.38164474867E11, :ISO-8601 "PT3M58.164474867S"}]
     [ctia.entity.incident-test/test-incident-severity-int-search
      {:elapsed-ns 1.24112328665E11, :ISO-8601 "PT2M4.112328665S"}]
     [ctia.entity.weakness-test/test-weakness-routes-access-control
      {:elapsed-ns 1.22775289708E11, :ISO-8601 "PT2M2.775289708S"}]
     [ctia.entity.campaign-test/test-campaign-routes-access-control
      {:elapsed-ns 1.16010526277E11, :ISO-8601 "PT1M56.010526277S"}]
     [ctia.entity.incident-test/test-incident-routes-access-control
      {:elapsed-ns 1.1082183857E11, :ISO-8601 "PT1M50.82183857S"}]
     [ctia.entity.casebook-test/test-casebook-routes-access-control
      {:elapsed-ns 1.01860613086E11, :ISO-8601 "PT1M41.860613086S"}]
     [ctia.http.routes.pagination-test/test-pagination-lists
      {:elapsed-ns 1.01767739728E11, :ISO-8601 "PT1M41.767739728S"}]
     [ctia.entity.actor-test/test-actor-routes-access-control
      {:elapsed-ns 9.9887591013E10, :ISO-8601 "PT1M39.887591013S"}]
     [ctia.task.migration.migrate-es-stores-test/test-migrate-store-indexes
      {:elapsed-ns 9.8318897082E10, :ISO-8601 "PT1M38.318897082S"}]
     [ctia.entity.relationship-test/test-relationship-routes-access-control
      {:elapsed-ns 9.7785129821E10, :ISO-8601 "PT1M37.785129821S"}]
    ```
    <!--
    
    Describe your PR for reviewers.
    Don't forget to set correct labels (User Facing / Beta / Feature Flag)
    If there is UI change please add a screen capture.
    -->
    
    <!-- UNCOMMENT THIS SECTION IF NEEDED
    <a name="iroh-services-clients">[§](#iroh-services-clients)</a> IROH Services Clients
    =====================================================================================
    
    Put all informations that need to be communicated to IROH Services Clients.
    Typically IROH-UI, ATS Integration, Orbital, etc...
     -->
    
    <a name="qa">[§](#qa)</a> QA
    ============================
    
    <!--
    Describe the steps to test your PR.
    
    1.
    2.
    3.
    
    Or if no QA is needed keep it as is.
     -->
    No QA is needed.
    
    <!-- UNCOMMENT THIS SECTION IF NEEDED
    <a name="ops">[§](#ops)</a> Ops
    ===============================
    
      Specify Ops related issues and documentation
    - Config change needed: threatgrid/tenzin#
    - Migration needed: threatgrid/tenzin#
    - How to enable/disable that feature: (ex remove service from `bootstrap.cfg`, add scope to org)
    -->
    
    <!-- UNCOMMENT THIS SECTION IF NEEDED
    <a name="documentation">[§](#documentation)</a> Documentation
    =============================================================
    
      Public Facing documentation section;
    - Public documentation updated needed: threatgrid/iroh-ui#
      See internal [doc file](./services/iroh-auth/doc/public-doc.org)
     -->
    
    <a name="release-notes">[§](#release-notes)</a> Release Notes
    =============================================================
    
    <!-- REMOVE UNUSED LINES -->
    
    ```
    intern: Summarize test durations in minutes
    ```
    
    <a name="squashed-commits">[§](#squashed-commits)</a> Squashed Commits
    ======================================================================

--- scripts/summarize-tests.clj
+++ scripts/summarize-tests.clj
@@ -1,7 +1,8 @@
 #! /usr/bin/env bb
 
 (require '[clojure.pprint :as pp])
-(import '[java.io File])
+(import '[java.io File]
+        '[java.time Duration])
 
 (defn summarize []
   (let [timing-for-prefix (fn [file-prefix]
@@ -15,24 +16,25 @@
         ns-timing (timing-for-prefix "ns-timing")
         sorted-ns-timing (sort-by (comp :elapsed-ns val) > ns-timing)
         var-timing (timing-for-prefix "var-timing")
-        sorted-var-timing (sort-by (comp :elapsed-ns val) > var-timing)]
+        sorted-var-timing (sort-by (comp :elapsed-ns val) > var-timing)
+        humanize-ns (fn [ns]
+                      (str (Duration/ofNanos ns)))
+        humanize (fn [ts]
+                   (mapv (fn [[k {:keys [elapsed-ns] :as t}]]
+                           (assert (number? elapsed-ns) (pr-str t))
+                           [k (assoc t :ISO-8601 (humanize-ns elapsed-ns))])
+                         ts))]
     (when-some [expected (let [f (File. "dev-resources/ctia_test_timings.edn")]
                            (when (.exists f)
                              (-> f
                                  slurp
                                  read-string)))]
-      (println (str "Expected test duration: "
-                    (/ (apply + (map :elapsed-ns (vals expected)))
-                       1e9)
-                    " seconds")))
-    (println (str "Actual test duration: "
-                  (/ (apply + (map :elapsed-ns (vals ns-timing)))
-                     1e9)
-                  " seconds"))
+      (println (str "Expected test duration: " (humanize-ns (apply + (map :elapsed-ns (vals expected)))))))
+    (println (str "Actual test duration: " (humanize-ns (apply + (map :elapsed-ns (vals ns-timing))))))
     (println "\nTest namespace summary (slowest to fastest):")
-    (pp/pprint sorted-ns-timing)
+    (pp/pprint (humanize sorted-ns-timing))
     (println "\nTest var summary (slowest to fastest):")
-    (pp/pprint sorted-var-timing)
+    (pp/pprint (humanize sorted-var-timing))
     (-> (File. "target/test-results") .mkdirs)
     (spit "target/test-results/all-test-var-timings.edn" var-timing)
     (spit "target/test-results/all-test-timings.edn" ns-timing)))
