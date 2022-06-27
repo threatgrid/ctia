0beca18c2e625f122875dce89eab603e98f1398d
Kirill Chernyshov <kchernys@cisco.com>
2022-06-27 15:49:54 +0200

    Introduce handle-read-many
    Crud store should be able to fetch many documents by their ids in one ES
    query.

--- src/ctia/stores/es/crud.clj
+++ src/ctia/stores/es/crud.clj
@@ -206,6 +206,33 @@ It returns the documents with full hits meta data including the real index in wh
           (throw (ex-info "You are not allowed to read this document"
                           {:type :access-control-error})))))))
 
+(defn handle-read-many
+  "Generate an ES read-many handler using some mapping and schema"
+  [Model]
+  (let [coerce! (coerce-to-fn Model)]
+    (s/fn :- [(s/maybe Model)]
+      [{{{:keys [get-in-config]} :ConfigService}
+        :services
+        :as conn-state}
+       :- ESConnState
+       ids :- [s/Str]
+       ident
+       {:keys [suppress-access-control-error?]
+        :or {suppress-access-control-error? false}
+        :as es-params}]
+      (sequence
+       (comp (map :_source)
+             (map coerce!)
+             (map (fn [record]
+                    (if (allow-read? record ident get-in-config)
+                      record
+                      (let [ex (ex-info "You are not allowed to read this document"
+                                        {:type :access-control-error})]
+                        (if suppress-access-control-error?
+                          (log/error ex)
+                          (throw ex)))))))
+       (get-docs-with-indices conn-state ids (make-es-read-params es-params))))))
+
 (defn access-control-filter-list
   "Given an ident, keep only documents it is allowed to read"
   [docs ident get-in-config]
