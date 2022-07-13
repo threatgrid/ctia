72a9ad38d15f8f1318f9a0174a5c755b50e38305
Kirill Chernyshov <kchernys@cisco.com>
2022-07-13 10:46:07 +0200

    Remove debug log

--- containers/dev/elasticsearch5-password/entrypoint.sh
+++ containers/dev/elasticsearch5-password/entrypoint.sh
@@ -9,6 +9,9 @@ generate_post_data()
 EOF
 }
 
-echo "$(generate_post_data)"
-
-wait-for-it elasticsearch5:9200 --timeout=30 --strict -- curl -X POST -H "Content-Type: application/json" -d "$(generate_post_data)" --user "elastic:changeme" elasticsearch5:9200/_xpack/security/user/elastic/_password
+wait-for-it elasticsearch5:9200 --timeout=30 --strict -- \
+    curl -X POST \
+    -H "Content-Type: application/json" \
+    -d "$(generate_post_data)" \
+    --user "elastic:changeme" \
+    elasticsearch5:9200/_xpack/security/user/elastic/_password
