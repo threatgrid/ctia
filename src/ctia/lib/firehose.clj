b30a98b84cec991a429fcc6ee1416477d2e06245
bswanson <brookeswanson09@gmail.com>
2022-08-23 10:27:33 -0500

    Add default basic auth-client for testing.

--- src/ctia/lib/firehose.clj
+++ src/ctia/lib/firehose.clj
@@ -4,6 +4,7 @@
   (:import
    [java.net URI]
    [software.amazon.awssdk.core SdkBytes]
+   [software.amazon.awssdk.auth.credentials AwsBasicCredentials StaticCredentialsProvider]
    [software.amazon.awssdk.services.firehose.model
     PutRecordRequest PutRecordResponse Record FirehoseException]
    [software.amazon.awssdk.services.firehose FirehoseClient]))
@@ -12,12 +13,17 @@
   ^FirehoseClient []
   (FirehoseClient/create))
 
+
+;; for local testing
+(def local-basic-auth-client (AwsBasicCredentials/create "test" "test"))
+
 (defn local-client
   "For local testing with localstack for the firehose hook"
   ^FirehoseClient []
   (.build
    (doto (FirehoseClient/builder)
-     (.endpointOverride (URI. "http://localhost:4566")))))
+     (.endpointOverride (URI. "http://localhost:4566"))
+     (.credentialsProvider (StaticCredentialsProvider/create local-basic-auth-client)))))
 
 (defn get-client-fn
   "For local testing purposes. If you are using local stack while
