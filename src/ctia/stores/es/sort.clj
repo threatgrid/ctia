8647b23d52538cb47122444d41e5d339a2e0ce7c
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2022-08-25 13:51:40 -0400

    dedupe script, rm assertion, and move doc

--- src/ctia/stores/es/sort.clj
+++ src/ctia/stores/es/sort.clj
@@ -13,67 +13,58 @@
                              (string? %) str/lower-case)))
 
 (s/defn parse-sort-params-op
+  "Currently 3 operations are supported:
+
+
+  Sort by field value:
+      eg., sort on Severity, ascending:
+      {:op :field
+       :field-name \"Severity\"
+       :sort_order :asc}
+
+  Remap field value, then sort on remapping:
+      eg., remap severity to to integers, then sort ints:
+      {:op :remap
+       :field-name \"severity\"
+       :remappings {\"Critical\" 0
+                    \"High\" 1}
+       :sort_order :asc
+       :remap-default 0}
+
+  Remap field value (a list) to the maximum int in remappings, then sort on that:
+      eg. find maximum remapping of tactics (a list of strings) then sort ascending.
+      {:op :remap-list-max
+       :field-name \"tactics\"
+       :remappings {\"TA0043\" 1
+                    \"TA0042\" 2}
+       :sort_order :asc}"
   [{:keys [op field-name sort_order] :as params} :- ConcreteSortExtension
    default-sort_order :- (s/cond-pre s/Str s/Keyword)]
   (let [field-name (name field-name)
         order (keyword (or sort_order default-sort_order))]
     (assert (not (some #{"'"} field-name)) (pr-str field-name))
     (case op
-      ;; eg
-      ;; {:op :field
-      ;;  :field-name "Severity"
-      ;;  :sort_order :asc}
       :field
       ;; https://www.elastic.co/guide/en/elasticsearch/reference/current/sort-search-results.html#_sort_values
       {field-name {:order order}}
 
-      ;; chooses the maximum value in field (a list) based on remappings,
-      ;; and sorts based on that value for each entity. Remappings must
-      ;; be positive integers.
-      ;; eg
-      ;; {:op :remap-list-max
-      ;;  :field-name "tactics"
-      ;;  :remappings {"TA0043" 1
-      ;;               "TA0042" 2}
-      ;;  :sort_order :asc}
-      :remap-list-max
-      (let [{:keys [remap-default remappings]} params
-            remappings (normalize-remappings remappings)]
-        (assert (every? (complement neg?) (vals remappings)) ":remap-list-max :remappings values must be non-negative")
-        {:_script
-         {:type "number"
-          :script {:lang "painless"
-                   :inline (str/join
-                             "\n"
-                             ["if (!doc.containsKey(params.fieldName)) { return params.default }"
-                              "double score = params.default;"
-                              "for (int i = 0; i < doc[params.fieldName].length; ++i) {"
-                              "  score = Math.max(score, params.remappings.getOrDefault(doc[params.fieldName][i], params.default));"
-                              "}"
-                              "return score;"])
-                   :params {:remappings remappings
-                            :fieldName field-name
-                            :default remap-default}}
-          :order order}})
-
-      ;; eg
-      ;; {:op :remap
-      ;;  :field-name "severity"
-      ;;  :remappings {"Critical" 0
-      ;;               "High" 1}
-      ;;  :sort_order :asc
-      ;;  :remap-default 0}
-      :remap
+      (:remap :remap-list-max)
       (let [{:keys [remap-default remappings]} params
             remappings (normalize-remappings remappings)]
         ;; https://www.elastic.co/guide/en/elasticsearch/painless/current/painless-sort-context.html
         {:_script
          {:type "number"
           :script {:lang "painless"
-                   :inline (str/join
-                             "\n"
-                             ["if (!doc.containsKey(params.fieldName) || doc[params.fieldName].size() != 1) { return params.default }"
-                              "return params.remappings.getOrDefault(doc[params.fieldName].value, params.default)"])
+                   :inline (str/join "\n"
+                                     (case op
+                                       :remap ["if (!doc.containsKey(params.fieldName) || doc[params.fieldName].size() != 1) { return params.default }"
+                                               "return params.remappings.getOrDefault(doc[params.fieldName].value, params.default)"]
+                                       :remap-list-max ["if (!doc.containsKey(params.fieldName)) { return params.default }"
+                                                        "double score = params.default;"
+                                                        "for (int i = 0; i < doc[params.fieldName].length; ++i) {"
+                                                        "  score = Math.max(score, params.remappings.getOrDefault(doc[params.fieldName][i], params.default));"
+                                                        "}"
+                                                        "return score;"]))
                    :params {:remappings remappings
                             :fieldName field-name
                             :default remap-default}}
