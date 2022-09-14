4261017ccdc726652a83d7166935a88c88eac526
Kirill Chernyshov <kchernys@cisco.com>
2022-09-14 13:04:22 +0200

    Revert optimisations for bundle export (#1303)
    > Related 7cd8fe176b49036fd78285e2fa4dd80580d1ea42
    
    This PR reverts changes for the bundle export function. After deployment on production, there were a few problems related to those optimisations. As a first step to handling those problems, it is better to revert the changes and rethink the optimisation strategy.
    [commit](https://github.com/threatgrid/ctia/commit/7cd8fe176b49036fd78285e2fa4dd80580d1ea42#diff-20cea95187ae74e5a3f1cfd3770299e92b9ee526c8bf6cbdb5c791fd41627e69)
    
    <a name="qa">[§](#qa)</a> QA
    ============================
    
    1. Import that bundle [bundle-0.json.zip](https://github.com/threatgrid/ctia/files/8463390/bundle-0.json.zip). It contains two sightings. Note their ids after a successful import.
    2. Replace in every file from that archive [bundles.zip](https://github.com/threatgrid/ctia/files/8463400/bundles.zip) all occurrences of substrings `[[0]]` and `[[1]]` with sighting's ids from the result of step 1.
    3. Import all 5 bundles.
    4. Call bundle export with ids from the result of step 1. It should retrieve corresponding sightings together with related indicators. The result should not contain more than 1000 indicators per sighting.
    
    <a name="squashed-commits">[§](#squashed-commits)</a> Squashed Commits
    ======================================================================

--- test/ctia/bundle/core_test.clj
+++ test/ctia/bundle/core_test.clj
@@ -26,38 +26,37 @@
 
 (deftest relationships-filters
   (testing "relationships-filters should properly add related_to filters to handle edge direction"
-    (is (= "source_ref:(\"id\")"
-           (:query (sut/relationships-filter [{:id "id"}]
-                                             :source_ref
-                                             nil nil))))
-    (is (= "target_ref:(\"id\")"
-           (:query (sut/relationships-filter [{:id "id"}]
-                                             :target_ref
-                                             nil nil))))
-    (is (= "target_ref:(\"id1\" OR \"id2\")"
-           (:query (sut/relationships-filter [{:id "id1"} {:id "id2"}]
-                                             :target_ref
-                                             nil nil)))))
+    (is (= {:source_ref "id"
+            :target_ref "id"}
+           (:one-of (sut/relationships-filters "id" {})))
+        "default related-_to param is #{:source_ref :target_ref}")
+    (is (= {:source_ref "id"}
+           (:one-of (sut/relationships-filters "id" {:related_to [:source_ref]}))))
+    (is (= {:target_ref "id"}
+           (:one-of (sut/relationships-filters "id" {:related_to [:target_ref]}))))
+    (is (= {:source_ref "id"
+            :target_ref "id"}
+           (:one-of (sut/relationships-filters "id" {:related_to [:source_ref :target_ref]})))))
 
   (testing "relationships-filters should properly add query filters"
-    (is (= "source_ref:(\"id1\" OR \"id2\") AND source_ref:*malware*"
-           (:query (sut/relationships-filter [{:id "id1"} {:id "id2"}]
-                                             :source_ref
-                                             [:malware] nil))))
-    (is (= "source_ref:(\"id1\" OR \"id2\") AND target_ref:*sighting*"
-           (:query (sut/relationships-filter [{:id "id1"} {:id "id2"}]
-                                             :source_ref
-                                             nil [:sighting]))))
-    (is (= "target_ref:(\"id1\" OR \"id2\") AND source_ref:*malware* AND target_ref:*sighting*"
-           (:query (sut/relationships-filter [{:id "id1"} {:id "id2"}]
-                                             :target_ref
-                                             [:malware] [:sighting]))))
+    (is (= "(source_ref:*malware*)"
+           (:query (sut/relationships-filters "id" {:source_type [:malware]}))))
+    (is (= "(target_ref:*sighting*)"
+           (:query (sut/relationships-filters "id" {:target_type [:sighting]}))))
+    (is (= "(target_ref:*sighting*) AND (source_ref:*malware*)"
+           (:query (sut/relationships-filters "id" {:source_type [:malware]
+                                                    :target_type [:sighting]}))))
+    (is (= "(source_ref:*malware* OR source_ref:*vulnerability*)"
+           (:query (sut/relationships-filters "id" {:source_type [:malware :vulnerability]}))))
 
-    (is (= "target_ref:(\"id1\" OR \"id2\") AND (source_ref:*malware* OR source_ref:*vulnerability*) AND (target_ref:*sighting* OR target_ref:*incident*)"
-           (:query (sut/relationships-filter [{:id "id1"} {:id "id2"}]
-                                             :target_ref
-                                             [:malware :vulnerability]
-                                             [:sighting :incident]))))))
+    (is (= "(target_ref:*sighting* OR target_ref:*incident*)"
+           (:query (sut/relationships-filters "id" {:target_type [:sighting :incident]})))))
+
+  (testing "relationships-filters should return proper fields and combine filters"
+    (is (= {:one-of {:source_ref "id"}
+            :query "(source_ref:*malware*)"}
+           (sut/relationships-filters "id" {:source_type [:malware]
+                                            :related_to [:source_ref]})))))
 
 (deftest with-existing-entity-test
   (testing "with-existing-entity"
