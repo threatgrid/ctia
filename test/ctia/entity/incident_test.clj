5fe2c684b4bad20538a285513bb2764db6ce909a
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2022-10-18 09:05:07 -0500

    Filter incidents by tactics (#1314)
    * wip
    
    * fix
    
    * strengthen tests
    
    * query

--- test/ctia/entity/incident_test.clj
+++ test/ctia/entity/incident_test.clj
@@ -401,3 +401,40 @@
                        true
                        true
                        test-for-each-store-with-app))
+
+(deftest filter-incidents-by-tactics-test
+  (es-helpers/for-each-es-version
+    "sort by tactics"
+    [5 7]
+    #(ductile.index/delete! % "ctia_*")
+    (helpers/with-properties (into ["ctia.auth.type" "allow-all"]
+                                   es-helpers/basic-auth-properties)
+      (helpers/fixture-ctia-with-app
+        (fn [app]
+          ;(helpers/set-capabilities! app "foouser" ["foogroup"] "user" all-capabilities)
+          ;(whoami-helpers/set-whoami-response app "45c1f5e3f05d0" "foouser" "foogroup" "user")
+          (try (let [incident1 (assoc (gen-new-incident) :tactics ["TA0002" "TA0043" "TA0006"])
+                     incident2 (assoc (gen-new-incident) :tactics ["TA0004" "TA0043" "TA0008"])
+                     incident3 (assoc (gen-new-incident) :tactics ["TA0008" "TA0043" "TA0006" "TA8888"])
+                     normalize (fn [incidents]
+                                 (->> incidents
+                                      (map #(select-keys % [:title :tactics]))
+                                      (sort-by :tactics)))]
+                 (create-incidents app #{incident1 incident2 incident3})
+                 (testing "incident1"
+                   (let [{:keys [parsed-body] :as raw} (search-th/search-raw app :incident {:query "tactics:(\"TA0002\")"})]
+                     (and (is (= 200 (:status raw)) (pr-str raw))
+                          (is (= (normalize [incident1])
+                                 (normalize parsed-body))
+                              (pr-str parsed-body)))))
+                 (testing "incident1+2+3"
+                   (let [{:keys [parsed-body] :as raw} (search-th/search-raw app :incident {:query "tactics:(\"TA0043\")"})]
+                     (and (is (= 200 (:status raw)) (pr-str raw))
+                          (is (= (normalize [incident1 incident2 incident3])
+                                 (normalize parsed-body))))))
+                 (testing "incident1+3 multi"
+                   (let [{:keys [parsed-body] :as raw} (search-th/search-raw app :incident {:query "tactics:(\"TA0002\" || \"TA8888\")"}) ]
+                     (and (is (= 200 (:status raw)) (pr-str raw))
+                          (is (= (normalize [incident1 incident3])
+                                 (normalize parsed-body)))))))
+               (finally (purge-incidents! app))))))))
