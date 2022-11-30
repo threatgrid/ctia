03dff3aaf5298c459491934d6a3499e6cc0a4d3e
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2022-11-30 15:02:00 -0500

    mv

--- test/ctia/test_helpers/es.clj
+++ test/ctia/test_helpers/es.clj
@@ -100,10 +100,10 @@
         (:params basic-auth)))
 
 (defn -es-port []
-  (if ((h/set-of-es-versions-to-test) 7) "9207" (throw (ex-info "No ES version to test" {}))))
+  (if ((h/set-of-es-versions-to-test) 7) "9207" "9208"))
 
 (defn -es-version []
-  (if ((h/set-of-es-versions-to-test) 7) 7 (throw (ex-info "No ES version to test" {}))))
+  (if ((h/set-of-es-versions-to-test) 7) 7 8))
 
 (defn fixture-properties:es-store [t]
   ;; Note: These properties may be overwritten by ENV variables
