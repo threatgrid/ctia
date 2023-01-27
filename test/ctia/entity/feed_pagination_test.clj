8c4093496c2016d66d106bfb8dc41cb0934694a9
Kirill Chernyshov <kchernys@cisco.com>
2023-01-27 15:40:47 +0100

    Default "no-pagination" for feed (#1336)
    Return all related entities in case of no explicit pagination params.
    
    <a name="qa">[§](#qa)</a> QA
    ============================
    
    1. Import a single indicator and a lot of judgements (more than 10000) related to that indicator via related relationships with `"source_ref": "%indicator-id%"`
    2. Create a feed using indicator id from the result of the first step
    3. Get a view of the feed `GET /ctia/feed/:feed-id/view?s=:secret`. The response should contain all related judgements without pagination headers.
    4. Get a view of the feed `GET /ctia/feed/:feed-id/view?s=:secret&limit=11000`. The response should have 400 status code and validation error for limit query parameter.
    
    <a name="release-notes">[§](#release-notes)</a> Release Notes
    =============================================================
    
    ```
    intern: one-line description for internal eyes only.
    public: one line description for public release notes.
    ```
    
    <a name="squashed-commits">[§](#squashed-commits)</a> Squashed Commits
    ======================================================================
    
    * Default pagination for feed
    
    Return all related entities in case of no explicit pagination params.
    
    * Add test
    
    * Make feed pagination params strict

---
+++ test/ctia/entity/feed_pagination_test.clj
@@ -0,0 +1,152 @@
+(ns ctia.entity.feed-pagination-test
+  (:require
+   [clj-http.client :as client]
+   [clojure.edn :as edn]
+   [clojure.string :as string]
+   [clojure.test :refer [deftest is join-fixtures testing use-fixtures]]
+   [ctia.entity.feed :as sut]
+   [ctia.test-helpers.access-control :refer [access-control-test]]
+   [ctia.test-helpers.auth :refer [all-capabilities]]
+   [ctia.test-helpers.core :as helpers]
+   [ctia.test-helpers.crud :refer [entity-crud-test]]
+   [ctia.test-helpers.fake-whoami-service :as whoami-helpers]
+   [ctia.test-helpers.store :refer [test-for-each-store-with-app]]
+   [ctim.schemas.common :as c]
+   [schema.test :refer [validate-schemas]]))
+
+(use-fixtures :once
+  (join-fixtures [validate-schemas
+                  whoami-helpers/fixture-server]))
+
+(def indicator-id
+  "transient:esa-indicator-ec95b")
+
+(def indicator
+  {:description "" ,
+   :tags ["Suspicious IPs"],
+   :valid_time {:start_time "2019-05-03T21:48:25.801Z",
+                :end_time "2052-06-03T21:48:25.801Z"},
+   :producer "Talos",
+   :schema_version "1.0.11",
+   :type "indicator",
+   :source "Feed Indicator Example",
+   :external_ids ["ctia-feed-indicator-test"],
+   :short_description "Custom Suspicious IP Watchlist",
+   :title "Custom Suspicious IP Watchlist",
+   :indicator_type ["IP Watchlist"],
+   :source_uri "http://example.com/foo" ,
+   :id indicator-id,
+   :severity "High",
+   :tlp "amber",
+   :confidence "High"})
+
+(def base-judgement
+  {:schema_version "1.0.11",
+   :type "judgement",
+   :source "" ,
+   :external_ids ["ctia-feed-indicator-test"],
+   :disposition 2,
+   :source_uri "http://example.com/foo" ,
+   :disposition_name "Malicious",
+   :priority 95,
+   :severity "High",
+   :tlp "amber",
+   :timestamp "2019-03-01T19:22:45.531Z",
+   :confidence "High"})
+
+(def judgements
+  (map #(let [transient-id (format "transient:esa-judgement-%03d" %)
+              ip (format "187.75.42.%d" %)]
+          (into base-judgement {:id transient-id
+                                :valid_time {:start_time "2019-03-01T19:22:45.531Z",
+                                             :end_time "2052-03-31T19:22:45.531Z"}
+                                :observable {:type "ip" :value ip}}))
+       (range 100)))
+
+(def relationships
+  (map #(let [suffix (string/replace % #"transient:esa-judgement-" "")
+              transient-id (str "transient:esa-relationship-" suffix)]
+          {:id transient-id
+           :source_ref %
+           :schema_version "1.0.11",
+           :target_ref indicator-id,
+           :type "relationship",
+           :external_ids ["ctia-feed-indicator-test"],
+           :short_description "" ,
+           :title "" ,
+           :external_references [],
+           :tlp "amber",
+           :timestamp "2019-05-08T18:03:32.785Z",
+           :relationship_type "element-of"})
+       (map :id judgements)))
+
+(def bundle
+  {:type "bundle",
+   :source "Feed Indicator Example",
+   :source_uri "https://github.com/threatgrid/ctim/blob/master/src/doc/tutorials/modeling-threat-intel-ctim.md",
+   :indicators [indicator],
+   :judgements judgements
+   :relationships relationships})
+
+(def new-feed-maximal
+  {:revision 0
+   :schema_version c/ctim-schema-version
+   :type "feed"
+   :tlp "green"
+   :timestamp #inst "2016-05-11T00:40:48.212-00:00"
+   :external_references [{:source_name "source"
+                          :external_id "T1067"
+                          :url "https://ex.tld/wiki/T1067"
+                          :hashes ["#section1"]
+                          :description "Description text"}]
+   :external_ids ["https://ex.tld/ctia/feed/feed-345"]
+   :indicator_id "https://ex.tld/ctia/indicator/indicator-345"
+   :language "en"
+   :title "title"
+   :feed_type "indicator"
+   :output :observables
+   :lifetime {:start_time #inst "2016-01-01T01:01:01.000Z"
+              :end_time #inst "2042-01-01T01:01:01.000Z"}})
+
+(deftest test-feed-pagination
+  (test-for-each-store-with-app
+   (fn [app]
+     (helpers/set-capabilities! app
+                                "foouser"
+                                ["foogroup"]
+                                "user"
+                                all-capabilities)
+     (whoami-helpers/set-whoami-response app
+                                         "45c1f5e3f05d0"
+                                         "foouser"
+                                         "foogroup"
+                                         "user")
+     (with-redefs [ductile.pagination/max-result-window 40]
+       (let [response (helpers/POST app
+                          "ctia/bundle/import"
+                          :body bundle
+                          :headers {"Authorization" "45c1f5e3f05d0"})
+             bundle-import-result (:parsed-body response)
+             indicator-id (some->> (:results bundle-import-result)
+                                   (filter #(= (:type %) :indicator))
+                                   first
+                                   :id)
+
+             {{feed-view-url :feed_view_url} :parsed-body}
+             (helpers/POST app "ctia/feed?wait_for=true"
+               :body (assoc new-feed-maximal
+                            :indicator_id indicator-id)
+               :headers {:Authorization "45c1f5e3f05d0"})]
+
+         (testing "when no pagination params - return entire collection"
+           (let [expected-response (into #{} (map #(-> % :observable :value)) judgements)
+                 response (let [{:keys [body]} (client/get feed-view-url)]
+                            (into #{} (string/split-lines body)))]
+             (is (= 100 (count response)))
+             (is (= response expected-response))))
+
+         (testing "requesting a limit above maximum returns validation error"
+           (let [{:keys [body status]} (client/get feed-view-url {:query-params {:limit 10001}
+                                                                  :throw-exceptions false})]
+             (is (= 400 status))
+             (is (= "{:errors {:limit \"(not (less-than-10000? 10001))\"}}" body)))))))))
