4bb48cb5018e32c14b0bdf15838432d1cdeb2416
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-04-18 19:15:42 -0400

    [skip ci]

--- .clj-kondo/ctia/stores/es/store.clj
+++ .clj-kondo/ctia/stores/es/store.clj
@@ -5,4 +5,4 @@
    _entity
    _stored-schema
    _partial-stored-schema]
-  `(clojure.core/defn ~(symbol (str "->" (name store-name))) [state#] @(promise)))
+  `(clojure.core/defn ~(symbol (str "->" (name store-name))) [state#] (while true)))
