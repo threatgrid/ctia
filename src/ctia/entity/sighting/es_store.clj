366f236532ddaa8419cde2739f939e2dd4264777
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-04-21 12:27:00 -0400

    es-store

--- src/ctia/entity/sighting/es_store.clj
+++ src/ctia/entity/sighting/es_store.clj
@@ -7,7 +7,7 @@
             [ctia.schemas.search-agg :refer [QueryStringSearchArgs]]
             [ctia.store :refer [IQueryStringSearchableStore ISightingStore IStore] :as store]
             [ctia.stores.es
-             [store :refer [close-connections!] :as es.store]
+             [store :refer [def-es-store close-connections!] :as es.store]
              [crud :as crud]
              [mapping :as em]
              [schemas :refer [ESConnState]]]
@@ -91,39 +91,12 @@
    :es-stored-schema ESStoredSighting
    :es-partial-stored-schema ESPartialStoredSighting})
 
-(def impls (es.store/es-store-impls :sighting StoredSighting PartialStoredSighting store-opts))
-
-(defrecord SightingStore [state]
-  IStore
-  (read-record [_ id ident params]
-    ((:read-record impls) state id ident params))
-  (read-records [_ ids ident params]
-    ((:read-records impls) state ids ident params))
-  (create-record [_ new-sightings ident params]
-    ((:create-record impls) state new-sightings ident params))
-  (update-record [_ id sighting ident params]
-    ((:update-record impls) state id sighting ident params))
-  (delete-record [_ id ident params]
-    ((:delete-record impls) state id ident params))
-  (list-records [_ filter-map ident params]
-    ((:list-records impls) state filter-map ident params))
-  (bulk-delete [_ ids ident params]
-    (crud/bulk-delete state ids ident params))
-  (bulk-update [_ docs ident params]
-    ((:bulk-update impls) state docs ident params))
-  (close [_] (close-connections! state))
-  ISightingStore
-  (list-sightings-by-observables [this observables ident params]
-    (store/list-records this
-                        {:all-of {:observables_hash (obs->hashes observables)}}
-                        ident
-                        params))
-  IQueryStringSearchableStore
-  (query-string-search [_ args]
-    ((:query-string-search impls) state args))
-  (query-string-count [_ search-query ident]
-    (crud/handle-query-string-count state search-query ident))
-  (aggregate [_ search-query agg-query ident]
-    (crud/handle-aggregate state search-query agg-query ident))
-  (delete-search [_ search-query ident params]
-    (crud/handle-delete-search state search-query ident params)))
+(def-es-store SightingStore :sighting StoredSighting PartialStoredSighting
+  :store-opts store-opts
+  :extra-impls
+  [ISightingStore
+   (list-sightings-by-observables [this observables ident params]
+     (store/list-records this
+                         {:all-of {:observables_hash (obs->hashes observables)}}
+                         ident
+                         params))])
