83f5326d3c97dedf9f1d9e3792028d09106b04af
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-04-25 18:12:01 -0400

    fix

--- test/ctia/stores/es/crud_test.clj
+++ test/ctia/stores/es/crud_test.clj
@@ -338,7 +338,7 @@
                                                    :updated-field
                                                    "value1")
                                             ident
-                                            {}))))
+                                            {:refresh "wait_for"}))))
           (is (= "value1" (:updated-field (read-fn state-aliased (first ids) ident {}))))
           (is (true? (delete-fn state-aliased
                                 (first ids)
@@ -369,7 +369,7 @@
                                                      :updated-field
                                                      "value2")
                                               ident
-                                              {}))))
+                                              {:refresh "wait_for"}))))
             (is (= "value2" (:updated-field (read-fn state-aliased (second ids) ident {}))))
             (is (= "value5"
                    (:updated-field (update-fn state-aliased
@@ -378,7 +378,7 @@
                                                      :updated-field
                                                      "value5")
                                               ident
-                                              {}))))
+                                              {:refresh "wait_for"}))))
             (is (= "value5" (:updated-field (read-fn state-aliased (first ids) ident {}))))
             (is (true? (delete-fn state-aliased
                                   (second ids)
@@ -415,7 +415,7 @@
                                                   :updated-field
                                                   "value1")
                                            ident
-                                           {}))))
+                                           {:refresh "wait_for"}))))
          (is (= "value1" (:updated-field (read-fn state-not-aliased (first ids) ident {}))))
          (is (true? (delete-fn state-not-aliased
                                (first ids)
