a740cc0eb26254f6d023e4c88e090bc7472667fb
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-07-26 17:20:25 -0400

    wip

--- dev/ctia/dev/min_stores.clj
+++ dev/ctia/dev/min_stores.clj
@@ -56,25 +56,57 @@
                  [k v])))
         (entity-test-namespaces)))
 
+(defn log [ent & args]
+  (assert (infer-these-stores ent))
+  (let [msg (with-out-str (apply prn ent (java.util.Date.) args))]
+    (spit (str "min-stores-" (name ent) ".txt")
+          msg
+          :append true)
+    (spit "min-stores-all.txt" msg :append true)))
+
 (defn find-minimal-stores []
   (let [;;remove me
         entity-crud-route-tests #(take 1 (entity-crud-route-tests))]
     (into {}
-          (map (fn [[k tst]]
-                 (some (fn [i]
-                         (prn "i" i)
-                         (some (fn [enabled-stores]
-                                 (when (enabled-stores k) ;;current entity must always be enabled
-                                   (prn "enabled-stores" enabled-stores)
-                                   (let [res (th/with-enabled-stores enabled-stores
-                                               #(t/run-test-var tst))
-                                         _ (prn "res" res)]
-                                     (when (t/successful? res)
-                                       [k enabled-stores]))))
-                               (map set (comb/combinations possible-stores-to-enable i))))
-                       (range 1 (inc (count possible-stores-to-enable))))))
-          (entity-crud-route-tests))))
+          (pmap (fn [[k tst]]
+                  (some (fn [i]
+                          (log k "i" i)
+                          (some (fn [enabled-stores]
+                                  (when (enabled-stores k) ;;current entity must always be enabled
+                                    (log k "enabled-stores" enabled-stores)
+                                    (let [res (let [out (new java.io.StringWriter)
+                                                    err (new java.io.StringWriter)]
+                                                (binding [*out* out
+                                                          *err* err]
+                                                  (let [res (th/with-enabled-stores enabled-stores
+                                                              #(t/run-test-var tst))]
+                                                    (log k "out" out)
+                                                    (log k "err" err)
+                                                    res)))
+                                          _ (log k "res" res)]
+                                      (if (t/successful? res)
+                                        (do (log k "GOOD" enabled-stores)
+                                            [k enabled-stores])
+                                        (do (log k "BAD" enabled-stores)
+                                            nil)))))
+                                (map set (comb/combinations possible-stores-to-enable i))))
+                        [(count possible-stores-to-enable)]
+                        #_
+                        (range 1 (inc (count possible-stores-to-enable)))))
+                (entity-crud-route-tests)))))
 
 (comment
   (find-minimal-stores)
   )
+
+; lein run -m ctia.dev.min-stores/-main &> LOG.txt
+; $ tail -f min-stores-all.txt
+(defn -main [& arg]
+  (require 'user)
+  ((requiring-resolve 'clojure.tools.namespace.repl/refresh))
+  (let [result (find-minimal-stores)]
+    (prn "RESULT" result)
+    (let [msg (with-out-str (prn "FINAL RESULT" result))]
+      (spit "RESULT.txt" msg :append true)
+      (spit "min-stores-all.txt" msg :append true))
+    (System/exit 0)))
