07da0dd264615a5c10c20b16a2f11168265bc670
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-09-13 12:37:52 -0500

    fix

--- test/ctia/bulk/core_test.clj
+++ test/ctia/bulk/core_test.clj
@@ -259,6 +259,7 @@
                                "visible entities that the user is not allowed to write on are returned as forbidden errors"))]
            (testing "bulk-patch shall properly patch submitties entitites"
              (let [other-group-res (sut/patch-bulk bulk-patch
+                                                   {}
                                                    other-group-ident
                                                    {:refresh "true"}
                                                    services)
