afc2391d089e1eea49f92bc2c1397cd2aee2408b
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-10-04 13:13:29 -0500

    rm :index schema

--- src/ctia/bulk/core.clj
+++ src/ctia/bulk/core.clj
@@ -60,9 +60,7 @@
                                       :else {:type (s/conditional
                                                      string? s/Str
                                                      :else s/Keyword)
-                                             :reason s/Str
-                                             (s/optional-key :index) s/Str
-                                             (s/optional-key :index_uuid) s/Str})
+                                             :reason s/Str})
             (s/optional-key :msg) s/Str
             (s/optional-key :entity) (s/pred map?)
             (s/optional-key :type) (s/conditional
