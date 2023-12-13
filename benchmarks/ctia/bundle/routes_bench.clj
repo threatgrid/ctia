d502356381f529a403de0e20dc59693d5fa1b780
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-12-12 23:35:27 -0600

    results

--- benchmarks/ctia/bundle/routes_bench.clj
+++ benchmarks/ctia/bundle/routes_bench.clj
@@ -13,6 +13,22 @@
   :setup (fn [] [(setup-ctia-es-store!)])
   :cleanup (fn [{:keys [app]}] (cleanup-ctia! app)))
 
+;; lein bench bundle
+;;
+;; Goal:  Import Bundle
+;; -----
+;; Case:  :empty-bundle-import-es-store
+;; Evaluation count : 110460 in 60 samples of 1841 calls.
+;;              Execution time mean : 1.854401 ms
+;;     Execution time std-deviation : 2.564676 ms
+;;    Execution time lower quantile : 631.847954 µs ( 2.5%)
+;;    Execution time upper quantile : 8.998960 ms (97.5%)
+;; 
+;; Found 7 outliers in 60 samples (11.6667 %)
+;;         low-severe       7 (11.6667 %)
+;;  Variance from outliers : 98.3202 % Variance is severely inflated by outliers
+
+
 (defn play [app fixture]
   (POST app
         "ctia/bundle/import"
