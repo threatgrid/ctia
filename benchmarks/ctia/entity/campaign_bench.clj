b7aa6a19e83c046b36207c8200201e9867784c98
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-12-13 15:47:25 -0600

    bench

--- benchmarks/ctia/entity/campaign_bench.clj
+++ benchmarks/ctia/entity/campaign_bench.clj
@@ -29,3 +29,30 @@
 
 (defcase create-campaign :small-campaign-es-store
   [{:keys [app]}] (play app small-campaign))
+
+;; lein bench campaign
+;;
+;; create-campaign
+;; Goal:  Create Campaign
+;; -----
+;; Case:  :big-campaign-es-store
+;; Evaluation count : 10380 in 60 samples of 173 calls.
+;;              Execution time mean : 6.625116 ms
+;;     Execution time std-deviation : 1.381886 ms
+;;    Execution time lower quantile : 5.072085 ms ( 2.5%)
+;;    Execution time upper quantile : 10.352341 ms (97.5%)
+;; 
+;; Found 2 outliers in 60 samples (3.3333 %)
+;;         low-severe       2 (3.3333 %)
+;;  Variance from outliers : 91.1637 % Variance is severely inflated by outliers
+;; 
+;; Case:  :small-campaign-es-store
+;; Evaluation count : 14760 in 60 samples of 246 calls.
+;;              Execution time mean : 4.696163 ms
+;;     Execution time std-deviation : 396.121872 µs
+;;    Execution time lower quantile : 4.052968 ms ( 2.5%)
+;;    Execution time upper quantile : 5.515621 ms (97.5%)
+;; 
+;; Found 1 outliers in 60 samples (1.6667 %)
+;;         low-severe       1 (1.6667 %)
+;;  Variance from outliers : 61.8652 % Variance is severely inflated by outliers
