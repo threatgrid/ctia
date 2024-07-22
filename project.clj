0dacfe558c17cf9ef44a1b7305f9dc6c190a13c4
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2024-07-22 14:45:11 -0500

    flanders

--- project.clj
+++ project.clj
@@ -86,7 +86,7 @@
                  ;; Schemas
                  [prismatic/schema "1.2.0"]
                  [metosin/schema-tools "0.12.2"]
-                 [threatgrid/flanders "0.1.23"]
+                 [threatgrid/flanders "1.0.2"]
                  [threatgrid/ctim "1.3.18"]
                  [instaparse "1.4.10"] ;; com.gfredericks/test.chuck > threatgrid/ctim
                  [threatgrid/clj-momo "0.3.5"]
