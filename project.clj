0eb514d28dbd5505233fdce02426ae2617f1c2f4
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2025-08-25 17:12:46 +0000

    bump

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 (def jackson-version "2.15.2")
 (def jackson-databind-version "2.15.2")
 (def metrics-clojure-version "2.10.0")
-(def netty-version "4.1.75.Final")
+(def netty-version "4.1.100.Final")
 (def perforate-version "0.3.4")
 (def ring-version "1.9.5")
 (def schema-generators-version "0.1.5")
